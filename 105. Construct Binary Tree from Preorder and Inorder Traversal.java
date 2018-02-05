c2e84863b1e8e57d430fe8ea1f4226d185647815
Qiyu Zhang <yuqizhang19@outlook.com>
2018-02-04 19:35:38 -0500

    Update 105. Construct Binary Tree from Preorder and Inorder Traversal.java

--- 105. Construct Binary Tree from Preorder and Inorder Traversal.java
+++ 105. Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -1,19 +1,21 @@
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return helper(0, 0, inorder.length - 1, preorder, inorder);
+        return helper(0, 0, inorder.length, preorder, inorder);
     }
-    public TreeNode helper(int preStart, int start, int end, int[] preorder, int[] inorder) {
-        if (start > end || preStart > preorder.length - 1) return null;
-        TreeNode root = new TreeNode(preorder[preStart]);
+    public TreeNode helper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
+        if (inStart > inEnd || preStart >= preorder.length) return null;
+        TreeNode cur = new TreeNode(preorder[preStart]);
         int index = 0;
-        for(int i = start; i < inorder.length; i++) {
-            if (inorder[i] == root.val) {
+        for (int i = inStart; i <= inEnd; i++) {
+            if (preorder[preStart] == inorder[i]) {
                 index = i;
                 break;
             }
         }
-        root.left = helper(preStart + 1, start, index - 1, preorder, inorder);
-        root.right = helper(preStart + (index - start) + 1, index + 1, end, preorder, inorder);
-        return root;
+        cur.left = helper(preStart + 1, inStart, index - 1, preorder, inorder); //下一个就是左子树的开头
+        cur.right = helper(preStart + (index - inStart) + 1, index + 1, inEnd, preorder, inorder);//借助inStart和inEnd来确定这个子树的大小
+                                      //加上大小后再移一位到新的子树，而inorder的下一个就是右子树
+        return cur;
     }
 }
+//hint:preorder的每个开头都是一个节点，在inorder中找到它就能确定一边子树的大小
