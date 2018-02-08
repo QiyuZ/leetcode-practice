36b56ce971a8dc17e776c911e64030b784cee6c5
Qiyu Zhang <yuqizhang19@outlook.com>
2018-02-08 18:46:27 -0500

    Create 524. Longest Word in Dictionary through Deleting.java

---
+++ 524. Longest Word in Dictionary through Deleting.java
@@ -0,0 +1,41 @@
+//不用sort
+class Solution {
+    public String findLongestWord(String s, List<String> d) {
+        String longest = "";
+        for (String dictWord : d) {
+            int i = 0;
+            for (char c : s.toCharArray()) 
+                if (i < dictWord.length() && c == dictWord.charAt(i)) i++;
+
+            if (i == dictWord.length() && dictWord.length() >= longest.length()) 
+                if (dictWord.length() > longest.length() || dictWord.compareTo(longest) < 0)
+                    longest = dictWord;
+        }
+        return longest;
+    }
+}
+
+//sort
+class Solution {
+    public String findLongestWord(String s, List<String> d) {
+        String res = "";
+        if (s == null || d.size() == 0) return "";
+        Collections.sort(d);
+        for (int i = 0; i < d.size(); i++) {
+            if (d.get(i).length() <= res.length()) continue;
+            if (canChange(s, d.get(i))) res = d.get(i);
+        }
+        return res;
+    }
+    public boolean canChange(String a, String b) {
+        int inda = 0, indb = 0;
+        while (inda < a.length() && indb < b.length()) {
+            if (a.charAt(inda) == b.charAt(indb)) {
+                inda++;
+                indb++;
+            } else inda++;
+        }
+        if (indb == b.length()) return true;
+        else return false;
+    }
+}
