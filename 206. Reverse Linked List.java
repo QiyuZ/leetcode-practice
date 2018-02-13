f1ddb807d040debe5b05a08af83f3e1fc18dcd84
Qiyu Zhang <yuqizhang19@outlook.com>
2018-02-13 15:21:15 -0500

    Create 206. Reverse Linked List.java

---
+++ 206. Reverse Linked List.java
@@ -0,0 +1,29 @@
+// iterative
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode newNode = null;//因为最后一位是null
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = newNode;
+            newNode = head;
+            next = head;
+        }
+    }
+}
+
+// recursive
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) return head;
+        return helper(head, null);
+    }
+    public ListNode helper(ListNode head, ListNode newNode) {
+        if (head == null) return newNode;
+        ListNode next = head.next;
+        head.next = newNode;
+        return helper(next, head);
+    }
+}
+//head依次取放到新链表的前面即可完成翻转
