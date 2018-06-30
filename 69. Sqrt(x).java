c058226e333190c2e5afccce24a7583cc3a34f62
Qiyu Zhang <yuqizhang19@outlook.com>
2018-06-29 20:46:46 -0400

    Create 69. Sqrt(x).java

---
+++ 69. Sqrt(x).java
@@ -0,0 +1,12 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x == 0) return 0;
+        int left = 1, right = x;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid > x / mid) right  = mid - 1; //说明取的mid过大
+            else left = mid + 1;
+        }
+        return right;
+    }
+}
