a4fbc083ce63a41d762b83fff6c3d75175b6e0ff
Qiyu Zhang <yuqizhang19@outlook.com>
2018-07-21 19:14:17 -0400

    Create 111. Minimum Depth of Binary Tree.java

---
+++ 111. Minimum Depth of Binary Tree.java
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        return (left == 0 || right == 0) ? left + right + 1 : Math.min(left, right) + 1;
+        //因为只要有一边还有分支那么深度就+1，所以即使left == 0 || right == 0，也要+1， 全不为零就找小的
+    }
+}
