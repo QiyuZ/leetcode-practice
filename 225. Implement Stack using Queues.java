18467609cfc958ac316e33e852db942790ce8095
Qiyu Zhang <yuqizhang19@outlook.com>
2018-08-08 18:00:17 -0400

    Create 225. Implement Stack using Queues.java

---
+++ 225. Implement Stack using Queues.java
@@ -0,0 +1,38 @@
+class MyStack {
+    Queue<Integer> queue;
+    /** Initialize your data structure here. */
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+    
+    /** Push element x onto stack. */
+    public void push(int x) {
+        int size = queue.size();
+        queue.offer(x);
+        for (int i = 0; i < size; i++) queue.offer(queue.poll()); //记录原来的个数，因为加进去的是在最后，所以不会出来，原先的顺序相反加到了最后
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return queue.poll();
+    }
+    
+    /** Get the top element. */
+    public int top() {
+        return queue.peek();
+    }
+    
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
