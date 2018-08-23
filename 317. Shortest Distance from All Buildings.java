ce076c55995f89bc69b9ece05304d2c5f3442649
Qiyu Zhang <yuqizhang19@outlook.com>
2018-08-23 19:38:33 -0400

    Create 317. Shortest Distance from All Buildings.java

---
+++ 317. Shortest Distance from All Buildings.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int shortestDistance(int[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
+        int m = grid.length, n = grid[0].length, house = 0;
+        int[][] reach = new int[m][n], dis = new int[m][n];
+        int[][] add = new int[][] {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    house++;
+                    int level = 1;
+                    Queue<int[]> queue = new LinkedList<>();
+                    queue.offer(new int[] {i, j});
+                    boolean[][] visited = new boolean[m][n];
+                    while (!queue.isEmpty()) {
+                        int size = queue.size();
+                        for (int q = 0; q < size; q++) {
+                            int[] cur = queue.poll();
+                            for (int k = 0; k < 4; k++) {
+                                int nx = cur[0] + add[k][0];
+                                int ny = cur[1] + add[k][1];
+                                if (nx >= 0 && nx < m && ny >= 0 && ny < n && !visited[nx][ny] && grid[nx][ny] == 0) {
+                                    visited[nx][ny] = true;
+                                    reach[nx][ny]++;
+                                    dis[nx][ny] += level;
+                                    queue.offer(new int[] {nx, ny});
+                                }
+                            }
+                        }
+                        level++; //每多一层，别忘了步数+1
+                    }
+                }
+            }
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0 && reach[i][j] == house) min = Math.min(min, dis[i][j]);
+            }
+        }
+        return min == Integer.MAX_VALUE ? -1 : min;
+    }
+}
+
+/* 每次从1（building）出发，计算到每个0的距离（dis[][]计算距离）并+1表示这个1可以到达这个0（reach[][]来计数），这样全部走一遍下来后，0的计数等于building总数则说明这个0可以作为中心点
+然后再比较谁的距离短 */
