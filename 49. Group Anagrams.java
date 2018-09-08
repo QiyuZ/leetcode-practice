753d3da04156fea8f36413c8bd0e23d0863d3150
Qiyu Zhang <yuqizhang19@outlook.com>
2018-09-08 16:46:25 -0400

    Update 49. Group Anagrams.java

--- 49. Group Anagrams.java
+++ 49. Group Anagrams.java
@@ -1,4 +1,21 @@
 class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) { //feature类型题，用map， feature做key下面各种变形做value
+        List<List<String>> res = new ArrayList<>();
+        if (strs == null || strs.length == 0) return res;
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            int[] words = new int[26];
+            for (char ch : str.toCharArray()) words[ch - 'a']++;
+            String key = Arrays.toString(words);
+            if (!map.containsKey(key)) map.put(key, new ArrayList<>());
+            map.get(key).add(str);
+        }
+        for (String k : map.keySet()) res.add(map.get(k));
+        return res;
+    }
+}
+
+class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> res = new ArrayList<>();
         if (strs == null || strs.length == 0) return res;
@@ -14,4 +31,3 @@ class Solution {
         return res;
     }
 }
-//以排好序的单词为
