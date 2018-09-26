a39a256ab139c56feb0a44de25a78b102d4c5e9c
Qiyu Zhang <yuqizhang19@outlook.com>
2018-09-26 17:39:06 -0400

    Update 200. Number of Islands.java

--- 200. Number of Islands.java
+++ 200. Number of Islands.java
@@ -22,3 +22,36 @@ class Solution {
         dfs(grid, i, j + 1);
     }
 }
+
+//union find
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
+        int count = 0, m = grid.length, n = grid[0].length;
+        int[] union = new int[m * n];
+        int[][] dirs = {{-1, 0}, {0, -1}};
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    union[i * n + j] = i * n + j;
+                    count++;
+                    for (int[] dir : dirs) {
+                        int x = i + dir[0], y = j + dir[1];
+                        if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] != '1') continue;
+                        int a = i * n + j, b = find(union, x * n + y);
+                        if (a != b) {
+                            count--;
+                            union[b] = a;
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    public int find(int[] union, int index) {
+        if (union[index] != index) return find(union, union[index]);
+        return index;
+    }
+}
