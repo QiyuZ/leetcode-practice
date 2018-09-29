a62ce46b2d5e37dd47804250a4e2e9c321ffd5f2
Qiyu Zhang <yuqizhang19@outlook.com>
2018-09-28 23:09:12 -0400

    Update 98. Validate Binary Search Tree.java

--- 98. Validate Binary Search Tree.java
+++ 98. Validate Binary Search Tree.java
@@ -1,12 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+//inorder traversal 左<根<y右
+//iteration
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        if (root == null) return null;
-        return helper(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
-    }
-    public boolean helper(TreeNode root, int lower, int upper) {
         if (root == null) return true;
-        if (root.val <= min || root.val >= upper) return false;
-        return helper(root.left, lower, root.val) && helper(root.right, root.val, upper);
-        //去了左边，parent node的val就是新的最大值， 去了左边，parent node的val就是新的最小值
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (pre != null && pre.val >= root.val) return false;
+            pre = root;
+            root = root.right;
+        }
+        return true;
+    }
+}
+
+//recursive
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    public boolean helper(TreeNode root, long min, long max) {
+        if (root == null) return true;
+        if (root.val >= max || root.val <= min) return false;
+        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
     }
 }
