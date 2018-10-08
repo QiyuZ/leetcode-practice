05816c1b1984dbe21a93be3bfec345076725f6fc
Qiyu Zhang <yuqizhang19@outlook.com>
2018-10-08 01:07:21 -0400

    Create 669. Trim a Binary Search Tree.java

---
+++ 669. Trim a Binary Search Tree.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if (root == null) return null;
+        if (root.val < L) return trimBST(root.right, L, R); //过小向右
+        else if (root.val > R) return trimBST(root.left, L, R);//过大向左
+        else { //在范围内就继续走
+            root.left = trimBST(root.left, L, R);
+            root.right = trimBST(root.right, L, R);
+            return root;
+        }
+    }
+}
