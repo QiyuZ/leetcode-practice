3978b0535aeeaf85b18b505a9c03dab12aba4826
Qiyu Zhang <yuqizhang19@outlook.com>
2018-10-21 21:35:49 -0400

    Create 116. Populating Next Right Pointers in Each Node.java

---
+++ 116. Populating Next Right Pointers in Each Node.java
@@ -0,0 +1,11 @@
+public class Solution { //这个保证两个children,117不保证
+    public void connect(TreeLinkNode root) {
+        if (root == null) return;
+        if (root.left != null) {
+            root.left.next = root.right;
+            if (root.next != null) root.right.next = root.next.left;
+        }
+        connect(root.left);
+        connect(root.right);
+    }
+}
