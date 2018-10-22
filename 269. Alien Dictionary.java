fd5c25a4364fed04b59220ad50f3249ac2e96313
Qiyu Zhang <yuqizhang19@outlook.com>
2018-10-22 12:36:32 -0400

    Update 269. Alien Dictionary.java

--- 269. Alien Dictionary.java
+++ 269. Alien Dictionary.java
@@ -1,44 +1,47 @@
 class Solution {
     public String alienOrder(String[] words) {
         if (words == null || words.length == 0) return "";
-        int[] degree = new int[26];
-        Map<Character, Set<Character>> map = new HashMap<>();
-        int count = 0;
-        StringBuilder res = new StringBuilder();
+        int[] indegree = new int[26];
+        Arrays.fill(indegree, -1); //初始化-1
         for (String word : words) {
             for (char c : word.toCharArray()) {
-                if (degree[c - 'a'] == 0) {
-                    count++;
-                    degree[c - 'a'] = 1; //标记出来哪几个字母出现
-                }
+                indegree[c - 'a'] = 0; //出现过的置为0
             }
         }
+        Map<Character, Set<Character>> map = new HashMap<>();
         for (int i = 0; i < words.length - 1; i++) {
             char[] cur = words[i].toCharArray();
             char[] next = words[i + 1].toCharArray();
-            int len = Math.min(cur.length, next.length); //找最小的因为要对应每个上下字母比较
+            int len = Math.min(cur.length, next.length);
             for (int j = 0; j < len; j++) {
-                if (cur[j] != next[j]) {
-                    if (!map.containsKey(cur[j]))  map.put(cur[j], new HashSet<Character>());
-                    if (map.get(cur[j]).add(next[j])) degree[next[j] - 'a']++; //找出怎么连接的，被连接的入度+1
-                    break; //找到最前面的第一个不相等的就不用往后看了
+                if (next[j] != cur[j]) { //注意，一定要是不相等的
+                    if (!map.containsKey(cur[j])) map.put(cur[j], new HashSet<>());
+                    if (!map.get(cur[j]).contains(next[j])) {
+                        map.get(cur[j]).add(next[j]);
+                        indegree[next[j] - 'a']++; //别忘了入度+1
+                    }
+                    break; //break很重要，因为只有第一个不同的是决定顺序的
                 }
             }
         }
+        StringBuilder sb = new StringBuilder();
         Queue<Character> queue = new LinkedList<>();
-        for (int i = 0; i < 26; i++) {
-            if (degree[i] == 1) queue.offer((char)('a' + i)); //如果degree=1说明是最初标记的，是起始点，应该只有一个
+        for (int i = 0; i < indegree.length; i++) {
+            if (indegree[i] == 0) queue.offer((char)(i + 'a')); //找到初始位置
         }
         while (!queue.isEmpty()) {
-            char out = queue.poll();
-            res.append(out);
-            if (map.containsKey(out)) {
-                for (char sub : map.get(out)) {
-                    if (--degree[sub - 'a'] == 1) queue.offer(sub); //除去它的上节点，如果入度变为1说明这是最新的起点
+            char cur = queue.poll();
+            sb.append(cur);
+            if (map.containsKey(cur)) {
+                for (char next : map.get(cur)) {
+                    indegree[next - 'a']--; //因为已经把当前的取出来了，所以它的孩子入度都要-1
+                    if (indegree[next - 'a'] == 0) queue.offer(next);//如果此时入度变为0，说明这个成为新的初始
                 }
             }
         }
-        if (res.length() == count) return res.toString(); //count这里使用，不相等有重复出现的，则不对
-        return "";
+        for (int num : indegree) { //检查是否都取出
+            if (num > 0) return "";
+        }
+        return sb.toString();
     }
 }
