b81c5774e9aa7cd5e3fec0b24b3abd685ee8eb02
Qiyu Zhang <yuqizhang19@outlook.com>
2018-10-26 16:26:52 -0400

    Update 98. Validate Binary Search Tree.java

--- 98. Validate Binary Search Tree.java
+++ 98. Validate Binary Search Tree.java
@@ -13,17 +13,17 @@
 class Solution {
     public boolean isValidBST(TreeNode root) {
         if (root == null) return true;
+        long pre = Long.MIN_VALUE;
         Stack<TreeNode> stack = new Stack<>();
-        TreeNode pre = null;
-        while (root != null || !stack.isEmpty()) {
+        while (!stack.isEmpty() || root != null) {
             while (root != null) {
                 stack.push(root);
                 root = root.left;
             }
-            root = stack.pop();
-            if (pre != null && pre.val >= root.val) return false;
-            pre = root;
-            root = root.right;
+            TreeNode cur = stack.pop();
+            if (cur.val <= pre) return false;
+            root = cur.right;
+            pre = cur.val;
         }
         return true;
     }
