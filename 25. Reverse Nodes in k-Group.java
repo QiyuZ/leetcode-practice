f790ca522316ee0efcd763e8fd878f0e6420bbf3
Qiyu Zhang <yuqizhang19@outlook.com>
2018-10-27 00:26:54 -0400

    Update 25. Reverse Nodes in k-Group.java

--- 25. Reverse Nodes in k-Group.java
+++ 25. Reverse Nodes in k-Group.java
@@ -6,26 +6,47 @@
  *     ListNode(int x) { val = x; }
  * }
  */
-public class Solution {
+class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        if (head == null || head.next == null) return head;
-        ListNode cur = head;
+        if (head == null) return head;
         int count = 0;
-        while (cur != null && count != k) {
+        ListNode cur = head;
+        while (count != k && cur != null) { //找下个group的初始节点
             cur = cur.next;
             count++;
         }
-        if (count == k) {
-            cur = reverseKGroup(cur, k);
-            while (count > 0) {
-                ListNode next = head.next;
-                head.next = cur;
-                cur = head;
-                head = next;
-                count--;
-            }
-            head = cur;
+        if (count != k) return head;
+        cur = reverseKGroup(cur, k);
+        while (count > 0) {
+            count--;
+            ListNode next = head.next;
+            head.next = cur;
+            cur = head;
+            head = next;
         }
-        return head;
+        return cur;
+    }
+}
+
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null) return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode start = dummy;
+        while (true) {
+            ListNode p = start;
+            ListNode c, n = p;
+            start = p.next;
+            for(int i = 0; i < k && n != null; i++) n = n.next;
+            if(n == null) break;
+            for(int i = 0; i < k-1; i++) {
+                c = p.next;
+                p.next = c.next;
+                c.next = n.next;
+                n.next = c;
+            }
+        }
+        return dummy.next;
     }
 }
