643b5724836e5f59c6d97c9b192740d32b7684fb
Qiyu Zhang <yuqizhang19@outlook.com>
2018-10-30 22:34:27 -0400

    Create 852. Peak Index in a Mountain Array.java

---
+++ 852. Peak Index in a Mountain Array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] A) {
+        int l = 0, r = A.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (A[mid] < A[mid + 1]) l = mid + 1; //mid+1大说明它可能是
+            else r = mid;
+        }
+        return l;
+    }
+}
