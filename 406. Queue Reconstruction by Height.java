b578652cdbf981e8194e00f4880231716e7dc569
Qiyu Zhang <yuqizhang19@outlook.com>
2018-11-17 22:16:26 -0500

    Update 406. Queue Reconstruction by Height.java

--- 406. Queue Reconstruction by Height.java
+++ 406. Queue Reconstruction by Height.java
@@ -2,7 +2,9 @@ class Solution {
     public int[][] reconstructQueue(int[][] people) {
         if (people == null || people.length == 0) return people;
         Arrays.sort(people, new Comparator<int[]>() {
-            return o1[0] != o2[0] ? o2[0] - o1[0] : o1[1] - o2[1];
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] != o2[0] ? o2[0] - o1[0] : o1[1] - o2[1];
+            }
         });
         List<int[]> res = new ArrayList<>();
         for (int[] p : people) res.add(p[1], p); //这个厉害了，因为先插入的是高的，所以这里可以直接排入后面的
