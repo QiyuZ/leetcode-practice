6f9208113d285474cbf4153f9a9e82d0e155d478
Qiyu Zhang <yuqizhang19@outlook.com>
2019-05-17 20:23:24 -0400

    Create 768. Max Chunks To Make Sorted II.java

---
+++ 768. Max Chunks To Make Sorted II.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int n = arr.length;
+        int[] maxOfLeft = new int[n];
+        int[] minOfRight = new int[n];
+
+        maxOfLeft[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            maxOfLeft[i] = Math.max(maxOfLeft[i-1], arr[i]); //从左边起最大的
+        }
+
+        minOfRight[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            minOfRight[i] = Math.min(minOfRight[i + 1], arr[i]); //从右边最小的
+        }
+
+        int res = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (maxOfLeft[i] <= minOfRight[i + 1]) res++; //成chunk的规则，右边最小的不能在左边的里面，反之就要多分块儿
+        }
+
+        return res + 1; //最右边那一块或者都不符合就单独一块
+    }
+}
