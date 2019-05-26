7258f79b2ba03ecdee961fa3b7bb260d4e4057ce
Qiyu Zhang <yuqizhang19@outlook.com>
2019-05-26 14:01:31 -0400

    Create 794. Valid Tic-Tac-Toe State.java

---
+++ 794. Valid Tic-Tac-Toe State.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean validTicTacToe(String[] board) {
+        int turns = 0;
+        boolean xwin = false, owin = false;
+        int[] rows = new int[3], cols = new int[3];
+        int diag = 0, antidiag = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i].charAt(j) == 'X') {
+                    turns++; rows[i]++; cols[j]++;
+                    if (i == j) diag++;
+                    if (i + j == 2) antidiag++;
+                } else if (board[i].charAt(j) == 'O') {
+                    turns--; rows[i]--; cols[j]--;
+                    if (i == j) diag--;
+                    if (i + j == 2) antidiag--;
+                }
+            }
+        }
+        xwin = rows[0] == 3 || rows[1] == 3 || rows[2] == 3 || 
+               cols[0] == 3 || cols[1] == 3 || cols[2] == 3 || 
+               diag == 3 || antidiag == 3;
+        owin = rows[0] == -3 || rows[1] == -3 || rows[2] == -3 || 
+               cols[0] == -3 || cols[1] == -3 || cols[2] == -3 || 
+               diag == -3 || antidiag == -3;
+        if ((xwin && turns != 1) || (owin && turns != 0)) return false;
+        return turns == 1 || turns == 0;
+    }
+}
