945ab73054dda9da7227eb17cb349717b87dfb0d
Qiyu Zhang <yuqizhang19@outlook.com>
2019-07-20 11:34:05 -0400

    Create 911. Online Election.java

---
+++ 911. Online Election.java
@@ -0,0 +1,25 @@
+class TopVotedCandidate {
+    TreeMap<Integer, Integer> map= new TreeMap<>();
+    public TopVotedCandidate(int[] persons, int[] times) {
+        int[] cnt = new int[persons.length];
+        int maxCnt = 0;
+        for (int i = 0; i < persons.length; i++){
+            int p= persons[i], t = times[i];
+            if (++cnt[p] >= maxCnt) { //In the case of a tie, the most recent vote (among tied candidates) wins.
+                maxCnt = cnt[p];
+                map.put(t, p);
+            }
+        }
+    }
+    
+    public int q(int t) {
+        int time = map.floorKey(t);
+        return map.get(time);
+    }
+}
+
+/**
+ * Your TopVotedCandidate object will be instantiated and called as such:
+ * TopVotedCandidate obj = new TopVotedCandidate(persons, times);
+ * int param_1 = obj.q(t);
+ */
