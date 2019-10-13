035b4ada2f8410308ac93aa83437dbe8123edaf0
Qiyu Zhang <yuqizhang19@outlook.com>
2019-10-13 13:14:53 -0400

    Update 1091. Shortest Path in Binary Matrix.java

--- 1091. Shortest Path in Binary Matrix.java
+++ 1091. Shortest Path in Binary Matrix.java
@@ -1,10 +1,10 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
         Queue<int[]> queue = new LinkedList<>();
-        Set<String> visited = new HashSet<>();
-        if (grid[0][0] == 1 || grid[grid.length - 1][grid[0].length - 1] == 1) return -1; //注意起点和终点不可以是1
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        if (grid[0][0] == 1 || grid[grid.length - 1][grid[0].length - 1] == 1) return -1;
         queue.offer(new int[] {0, 0});
-        visited.add(0 + "-" + 0);
+        visited[0][0] = true;
         int dirs[][] = new int[][]{{0,1},{0,-1},{1,0},{-1,0},{1,-1},{-1,1},{-1,-1},{1,1}};
         int path = 1;
         while (!queue.isEmpty()) {
@@ -14,9 +14,9 @@ class Solution {
                 if (cur[0] == grid.length - 1 && cur[1] == grid[0].length - 1) return path;
                 for (int[] d : dirs) {
                     int x = cur[0] + d[0], y = cur[1] + d[1];
-                    if (x >= grid.length || x < 0 || y < 0 || y >= grid[0].length || visited.contains(x + "-" + y) || grid[x][y] == 1) continue;
+                    if (x >= grid.length || x < 0 || y < 0 || y >= grid[0].length || visited[x][y] || grid[x][y] == 1) continue;
                     queue.offer(new int[] {x, y});
-                    visited.add(x + "-" + y);
+                    visited[x][y] = true;
                 }
             }
             path++;
@@ -24,4 +24,3 @@ class Solution {
         return -1;
     }
 }
-
