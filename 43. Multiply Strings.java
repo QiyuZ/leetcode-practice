1888c063f230071e9ed5858c70d7ec986a81d175
Qiyu Zhang <yuqizhang19@outlook.com>
2019-11-25 22:08:24 -0500

    Update 43. Multiply Strings.java

--- 43. Multiply Strings.java
+++ 43. Multiply Strings.java
@@ -1,21 +1,22 @@
 class Solution {
     public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) return "0";
         int len1 = num1.length(), len2 = num2.length();
-        int[] res = new int[len1 + len2 + 1]; //可能进一位
+        int[] res = new int[len1 + len2];
         for (int i = len1 - 1; i >= 0; i--) {
-            for (int j = len2 - 1; j >= 0; j--) {
-                int index = (len1 - 1) + (len2 - 1) - i - j;
-                res[index] += (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
-                res[index + 1] += res[index] / 10;
-                res[index] %= 10;
+            for (int j = len2 - 1; j>= 0; j--) {
+                int mult = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int back = i + j + 1, front = i + j;
+                int sum = res[back] + mult; //顶多是两位不会是3位，因为最大的就是9*9+9=90
+                res[back] = sum % 10;
+                res[front] += sum / 10;
             }
         }
         StringBuilder sb = new StringBuilder();
-        for (int i = res.length - 1; i > 0; i--) {
-            if (sb.length() == 0 && res[i] == 0) continue;
-            sb.append(res[i]);
+        for (int num : res) {
+            if (sb.length() == 0 && num == 0) continue;
+            sb.append(num);
         }
-        sb.append(res[0]); //第一个独立出去因为可能全是0那种
-        return sb.toString();
+        return sb.length() == 0 ? "0" : sb.toString();
     }
 }
