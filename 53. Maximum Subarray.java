07ae881f2fafa0fb1abfb1a85b7cd2dca29aebd1
Qiyu Zhang <yuqizhang19@outlook.com>
2019-12-05 21:00:09 -0500

    Update 53. Maximum Subarray.java

--- 53. Maximum Subarray.java
+++ 53. Maximum Subarray.java
@@ -9,3 +9,16 @@ class Solution {
         return max;
     }
 }
+
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0, res = Integer.MIN_VALUE;
+        for (int num : nums) {
+            if (sum < 0) sum = num; //已经是负的了，如果num>0则还不如自己单独成sum,如果num <0则越来越小
+            else sum += num;
+            res = Math.max(sum, res);
+        }
+        return res;
+    }
+}
