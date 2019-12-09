25701054049e990e3d0d190108e7c3f88f389fd4
Qiyu Zhang <yuqizhang19@outlook.com>
2019-12-09 01:17:05 -0500

    Update 295. Find Median from Data Stream.java

--- 295. Find Median from Data Stream.java
+++ 295. Find Median from Data Stream.java
@@ -1,20 +1,34 @@
 class MedianFinder {
-    PriorityQueue<Integer> min;
-    PriorityQueue<Integer> max;
+    PriorityQueue<Integer> descend;
+    PriorityQueue<Integer> ascend;
+    boolean even = true;
     /** initialize your data structure here. */
     public MedianFinder() {
-        min = new PriorityQueue<>();   
-        //巧妙利用两个queue，一个从小到大（默认）一个从大到小，保持从大到小的比另一个多一个或相等，这样中位数要么是max的peek要么就是两个peek的平均数
-        max = new PriorityQueue<>((a, b) -> b - a);
+        ascend = new PriorityQueue<>();
+        descend = new PriorityQueue<>((a, b) -> (b - a));
     }
     
     public void addNum(int num) {
-        max.offer(num);
-        min.offer(max.poll()); //移走大的到min
-        if (min.size() > max.size()) max.offer(min.poll()); //如果移走后max变少再移过来一个（注意不一定是同一个），保持max中元素多一个
+        if (even) { //even表示两个一样多，我们要让descend多一个，所以先放ascend再放入descend，反之同理
+            ascend.offer(num);
+            descend.offer(ascend.poll());
+        } else {
+            descend.offer(num);
+            ascend.offer(descend.poll());
+        }
+        even = !even;
     }
     
     public double findMedian() {
-        return min.size() == max.size() ? (min.peek() + max.peek()) / 2.0 : max.peek();
+        if (descend.size() > ascend.size()) return (double)descend.peek();
+        double sum = (double)(descend.peek() + ascend.peek());
+        return sum / 2;
     }
 }
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
