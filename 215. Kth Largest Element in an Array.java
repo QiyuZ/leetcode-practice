19fbf04d2544a7c895999e8c6fcb40e73407e779
Qiyu Zhang <yuqizhang19@outlook.com>
2019-12-15 17:45:04 -0500

    Update 215. Kth Largest Element in an Array.java

--- 215. Kth Largest Element in an Array.java
+++ 215. Kth Largest Element in an Array.java
@@ -1,26 +1,23 @@
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        return helper(nums, 0, nums.length - 1, nums.length - k);
-    }
-    
-    public int helper(int[] nums, int start, int end, int k) {
-        int pivot = nums[end], left = start;
-        for (int i = start; i < end; i++) {
-            if (nums[i] < pivot) {
-                swap(nums, i, left);
-                left++;
+    public int findKthLargest(int[] A, int k) {
+        k = A.length - k; // convert to index of k largest
+        int l = 0, r = A.length - 1;
+        while (l <= r) {
+            int i = l; // partition [l,r] by A[l]: [l,i]<A[l], [i+1,j)>=A[l]
+            for (int j = l + 1; j <= r; j++) {
+                if (A[j] < A[l]) swap(A, j, ++i);
             }
+            swap(A, l, i);
+            if (k < i) r = i - 1;
+            else if (k > i) l = i + 1;
+            else return A[i];
         }
-        swap(nums, left, end);
-        if (left == k) return nums[left];
-        else if (left > k) return helper(nums, start, left - 1, k);
-        else return helper(nums, left + 1, end, k);
+        return -1; // k is invalid
     }
-    
-    public void swap(int[] nums, int l, int r) {
-        int temp = nums[l];
-        nums[l] = nums[r];
-        nums[r] = temp;
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
 
