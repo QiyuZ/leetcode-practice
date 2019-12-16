d1a976f26aab82e2a684a6b922ce95415cad01fc
Qiyu Zhang <yuqizhang19@outlook.com>
2019-12-15 22:12:09 -0500

    Update 105. Construct Binary Tree from Preorder and Inorder Traversal.java

--- 105. Construct Binary Tree from Preorder and Inorder Traversal.java
+++ 105. Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -1,21 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 class Solution {
+    HashMap<Integer, Integer> inorderIndex = new HashMap<Integer, Integer>();
+    int pIndex = 0; //preIndex的顺序就是中左右，所以可以直接按照这个顺序来建树
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return helper(0, 0, inorder.length, preorder, inorder);
-    }
-    public TreeNode helper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
-        if (inStart > inEnd || preStart >= preorder.length) return null;
-        TreeNode cur = new TreeNode(preorder[preStart]);
-        int index = 0;
-        for (int i = inStart; i <= inEnd; i++) {
-            if (preorder[preStart] == inorder[i]) {
-                index = i;
-                break;
-            }
+        if (preorder.length == 0 || inorder.length == 0) return null;
+        for (int i = 0; i < inorder.length; i++) {
+            inorderIndex.put(inorder[i], i); //查询更快
         }
-        cur.left = helper(preStart + 1, inStart, index - 1, preorder, inorder); //下一个就是左子树的开头
-        cur.right = helper(preStart + (index - inStart) + 1, index + 1, inEnd, preorder, inorder);//借助inStart和inEnd来确定这个子树的大小
-                                      //加上大小后再移一位到新的子树，而inorder的下一个就是右子树
-        return cur;
+        return helper(0, preorder.length - 1, preorder);
+    }
+    private TreeNode helper(int left, int right, int[] preorder) {
+        if (left > right) return null;
+        int curr = preorder[pIndex++];
+        TreeNode root = new TreeNode(curr);
+        int index = inorderIndex.get(curr);
+        root.left = helper(left, index - 1, preorder); //划分左右
+        root.right = helper(index + 1, right, preorder);
+        return root;
     }
 }
-//hint:preorder的每个开头都是一个节点，在inorder中找到它就能确定一边子树的大小
