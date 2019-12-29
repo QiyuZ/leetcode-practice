48bb03c8b13dbc1a70876f5e6531918051fd5d4f
Qiyu Zhang <yuqizhang19@outlook.com>
2019-12-28 22:16:05 -0500

    Update 1236. Web Crawler.java

--- 1236. Web Crawler.java
+++ 1236. Web Crawler.java
@@ -29,3 +29,25 @@ class Solution {
         return url.split("/")[2];
     }
 }
+
+class Solution {
+    public List<String> crawl(String startUrl, HtmlParser htmlParser) {
+        if (startUrl == null || startUrl.length() == 0) return new ArrayList<>();
+        String hostName = getHostName(startUrl);
+        Set<String> visited = new HashSet<>();
+        dfs(startUrl, htmlParser, visited, hostName);
+        return new ArrayList<>(visited);
+    }
+    
+    private void dfs(String url, HtmlParser htmlParser, Set<String> visited, String host) {
+        if (visited.contains(url)) return;
+        visited.add(url);
+        for (String next : htmlParser.getUrls(url)) {
+            if (host.equals(getHostName(next))) dfs(next, htmlParser, visited, host);
+        }
+    }
+    
+    private String getHostName(String url) {
+        return url.split("/")[2];
+    }
+}
