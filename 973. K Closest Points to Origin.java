f033ef4bd7f1fe615fad0b3d5e6d9920f307855e
Qiyu Zhang <yuqizhang19@outlook.com>
2019-12-28 21:26:02 -0500

    Update 973. K Closest Points to Origin.java

--- 973. K Closest Points to Origin.java
+++ 973. K Closest Points to Origin.java
@@ -11,7 +11,35 @@ class Solution {
     }
 }
 
+public int[][] kClosest(int[][] points, int K) {
+    Arrays.sort(points, Comparator.comparing(p -> p[0] * p[0] + p[1] * p[1]));
+    return Arrays.copyOfRange(points, 0, K);
+}
+
+class Solution {
     public int[][] kClosest(int[][] points, int K) {
-        Arrays.sort(points, Comparator.comparing(p -> p[0] * p[0] + p[1] * p[1]));
+        if (points == null || points.length == 0 || K < 1 || K > points.length) return new int[][] {};
+        int l = 0, r = points.length - 1;
+        while (l <= r) {
+            int i = l;
+            for (int j = l + 1; j <= r; j++) {
+                if (compare(points[j], points[l]) < 0) swap(points, j, ++i);
+            }
+            swap(points, l, i);
+            if (K < i) r = i - 1;
+            else if (K > i) l = i + 1;
+            else break;
+        }
         return Arrays.copyOfRange(points, 0, K);
     }
+    
+    private int compare(int[] p1, int[] p2) {
+        return p1[0] * p1[0] + p1[1] * p1[1] - p2[0] * p2[0] - p2[1] * p2[1];
+    }
+
+    private void swap(int[][] nums, int i, int j) {
+        int[] temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
