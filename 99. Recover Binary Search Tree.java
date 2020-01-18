e997f43df49d080b95df3f4958546bb5c8c36172
Qiyu Zhang <yuqizhang19@outlook.com>
2020-01-17 21:35:27 -0500

    Update 99. Recover Binary Search Tree.java

--- 99. Recover Binary Search Tree.java
+++ 99. Recover Binary Search Tree.java
@@ -7,72 +7,35 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-
-//Morris遍历,不符合前面小于后面的，第一次遇到的取前面，第二次遇到的取后面
 class Solution {
+    TreeNode first = null, second = null, pre = new TreeNode(Integer.MIN_VALUE);
     public void recoverTree(TreeNode root) {
-        TreeNode first = null, second = null, pre = new TreeNode(Integer.MIN_VALUE);
-        boolean firstElement = true;
-        while (root != null) {
-            if (root.left != null) {
-                TreeNode temp = root.left;
-                while (temp.right != null && temp.right != root) temp = temp.right; //尽量走到子树的右节点
-                if (temp.right == null) { //走不动了如果是null说明第一次走到此处，建桥连接root, root可以左移了
-                    temp.right = root;
-                    root = root.left;
-                } else { //说明再一次走到此处，即已经建桥temp.right = root，这个时候可以取消这个连接,说明当前右边走不动了，visit
-                    temp.right = null;
-                    //visit root.val 每次换root前都要visit
-                    if (pre.val > root.val && firstElement) {//不符合前面小于后面的，第一次遇到的取前面
-                        first = pre;
-                        firstElement = false;
-                    }
-                    if (pre.val > root.val && !firstElement) second = root;  //第二次遇到的取后面
-                    pre = root;  //pre和root都要接着走
-                    root = root.right;
-                }
-            } else { //左边走不动了，说明到了最左边，visit
-                //visit root.val
-                if (pre.val >= root.val && firstElement) {
-                        first = pre;
-                        firstElement = false;
-                    }
-                if (pre.val >= root.val && !firstElement) second = root;
-                pre = root;
-                root = root.right;
-            }
-        }
-        if (first != null && second != null) { //交换
-            int temp = first.val;
-            first.val = second.val;
-            second.val = temp;
-        }
+        inorderTraverse(root);
+        swap(first, second);
     }
-}
-
-
-
-//空间为n的
-public class Solution {
     
-    TreeNode firstElement = null;
-    TreeNode secondElement = null;
-    TreeNode prevElement = new TreeNode(Integer.MIN_VALUE);
-    public void recoverTree(TreeNode root) {
-        traverse(root);
-        int temp = firstElement.val;
-        firstElement.val = secondElement.val;
-        secondElement.val = temp;
+    public void inorderTraverse(TreeNode root) {
+        if(root == null) return;
+        inorderTraverse(root.left);
+        //first wrong value will be bigger than current so first element = prev
+        if(first == null && pre.val > root.val) first = pre;
+        //second wrong value will be smaller than current so second element = root
+        if(first != null && pre.val > root.val) second = root; 
+        pre = root;
+        inorderTraverse(root.right);
     }
-    private void traverse(TreeNode root) {
-        if (root == null) return;  
-        traverse(root.left);
-        // Start of "do some business", 
-        // If first element has not been found, assign it to prevElement (refer to 6 in the example above)
-        if (firstElement == null && prevElement.val >= root.val) firstElement = prevElement;
-        // If first element is found, assign the second element to the root (refer to 2 in the example above)
-        if (firstElement != null && prevElement.val >= root.val) secondElement = root;
-        prevElement = root;
-        // End of "do some business"
-        traverse(root.right);
+    
+    private void swap(TreeNode first, TreeNode second) {
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
 }
+/*
+不可以写成
+if(pre.val > root.val) {
+    if (first == null) first = pre;
+    else second = pre;
+}
+这两个要同时因为可能并列 1274564 一个if就行，但是如果132这个时候就找不到2了
+*/
