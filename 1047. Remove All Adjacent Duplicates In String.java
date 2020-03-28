e21484ca000fb5fd53fb0a38a0256b26edbd43a8
Qiyu Zhang <yuqizhang19@outlook.com>
2020-03-28 14:24:00 -0400

    Update 1047. Remove All Adjacent Duplicates In String.java

--- 1047. Remove All Adjacent Duplicates In String.java
+++ 1047. Remove All Adjacent Duplicates In String.java
@@ -16,3 +16,16 @@ class Solution {
         return builder.toString();
     }
 }
+
+
+class Solution {
+    public String removeDuplicates(String S) {
+        char[] chars = S.toCharArray();
+        int i = -1;
+        for (int j = 0; j < chars.length; j++) {
+            if (i != -1 && chars[i] == chars[j]) i--;
+            else chars[++i] = chars[j];
+        }
+        return new String(chars, 0, i + 1);
+    }
+}
