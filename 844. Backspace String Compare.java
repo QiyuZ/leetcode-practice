2f0239339a61986e11a68d5a4a7bfd48703126a8
Qiyu Zhang <yuqizhang19@outlook.com>
2020-04-09 22:39:49 -0400

    Update 844. Backspace String Compare.java

--- 844. Backspace String Compare.java
+++ 844. Backspace String Compare.java
@@ -1,31 +1,32 @@
 class Solution {
-    public boolean backspaceCompare(String S, String T) { //O(1) time
+    public boolean backspaceCompare(String S, String T) {
         int i = S.length() - 1, j = T.length() - 1;
-        int skipi = 0, skipj = 0;
-        while (i >= 0 || j >= 0) {
+        int skip0 = 0, skip1 = 0;
+        while (i >= 0 || j >= 0) { //每次都停到字母上或者到了-1,要包括0因为0也要继续比
             while (i >= 0) {
-                if (S.charAt(i) == '#') { //先把#个数算出来，skip来减去前面的letter
-                    skipi++;
+                if (S.charAt(i) == '#') {
                     i--;
-                } else if (skipi > 0) {
+                    skip0++;
+                } else if (skip0 > 0) {
+                    skip0--;
                     i--;
-                    skipi--;
-                } else break; //最后停留在字母上
-            }
-            while (j >= 0) {
-                if (T.charAt(j) == '#') { //同理
-                    skipj++;
-                    j--;
-                } else if (skipj > 0) {
-                    j--;
-                    skipj--;
                 } else break;
             }
-            if (i >= 0 && j >= 0 && S.charAt(i) != T.charAt(j)) return false; //后面的能删除的都删了，当前不相等就肯定不行
-            if ((i >= 0) != (j >= 0)) return false; //一个结束了，另一个没有，而且最后一位是字母，也肯定不相等了
+            while (j >= 0) {
+                if (T.charAt(j) == '#') {
+                    j--;
+                    skip1++;
+                } else if (skip1 > 0) {
+                    skip1--;
+                    j--;
+                } else break;
+            }
+            if (i == -1 && j == -1) return true; //min index is -1
+            if (i == -1 || j == -1) return false; 
+            if (S.charAt(i) != T.charAt(j)) return false;
             i--;
-            j--;//移到下一位
+            j--;
         }
-        return true;//能出来就是true因为最后可能-1，或者-2等
+        return true;
     }
 }
