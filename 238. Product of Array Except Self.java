ae403b76045e95f9d61e2af0c3a8614de2672803
Qiyu Zhang <yuqizhang19@outlook.com>
2020-04-15 21:17:20 -0400

    Update 238. Product of Array Except Self.java

--- 238. Product of Array Except Self.java
+++ 238. Product of Array Except Self.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        if (nums == null || nums.length == 0) return nums;
         int[] res = new int[nums.length];
-        int temp = 1;
+        int multi = 1;
         for (int i = 0; i < nums.length; i++) {
-            res[i] = temp;
-            temp *= nums[i];
+            res[i] = multi;
+            multi *= nums[i];
         }
-        for (int i = nums.length - 1; i >= 0; i--) {
-            res[i] *= temp;
-            temp *= res[i];
+        multi = nums[nums.length - 1];
+        for (int i = nums.length - 2; i >= 0; i--) {
+            res[i] *= multi;
+            multi *= nums[i];
         }
         return res;
     }
 }
-//除了本身的乘积，那么就循环两次，第一次乘上这个数之前的，第二次乘上之后的
+//除了本身的乘积，那么就循环两次，第一次乘上这个数之前的，第二次乘上之后的, 一正一反避开自己
