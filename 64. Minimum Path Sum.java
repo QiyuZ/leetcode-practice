cc48bc9d709fc7c1e9edeedd49a9c9196122abe8
Qiyu Zhang <yuqizhang19@outlook.com>
2020-04-18 12:45:39 -0400

    Update 64. Minimum Path Sum.java

--- 64. Minimum Path Sum.java
+++ 64. Minimum Path Sum.java
@@ -1,16 +1,37 @@
-class Solution {
+class Solution { //dp
     public int minPathSum(int[][] grid) {
         if (grid == null || grid.length == 0) return 0;
-        int m = grid.length, n = grid[0].length;
-        int[][] dp = new int[m][n];
-        dp[0][0] = grid[0][0];
-        for (int i = 1; i < n; i++) dp[0][i] += dp[0][i - 1] + grid[0][i];
-        for (int i = 1; i < m; i++) dp[i][0] += dp[i - 1][0] + grid[i][0];
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[i][j] += Math.min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (i == 0 && j == 0) continue;
+                else if (i == 0) grid[i][j] += grid[i][j - 1];
+                else if (j == 0) grid[i][j] += grid[i - 1][j];
+                else grid[i][j] += Math.min(grid[i][j - 1], grid[i - 1][j]);
             }
         }
-        return dp[m - 1][n - 1];
+        return grid[grid.length - 1][grid[0].length - 1];
     }
 }
+
+// dfs timeout
+// class Solution {
+//     private int res = Integer.MAX_VALUE;
+//     public int minPathSum(int[][] grid) {
+//         if (grid == null || grid.length == 0) return 0;
+//         dfs(0, 0, grid, new boolean[grid.length][grid[0].length], 0);
+//         return res;
+//     }
+//     private void dfs(int i, int j, int[][] grid, boolean[][] visited, int sum) {
+//         if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || visited[i][j]) return;
+//         if (i == grid.length - 1 && j == grid[0].length - 1) {
+//             res = Math.min(res, sum + grid[i][j]);
+//             return;
+//         }
+//         visited[i][j] = true;
+//         dfs(i + 1, j, grid, visited, sum + grid[i][j]);
+//         dfs(i - 1, j, grid, visited, sum + grid[i][j]);
+//         dfs(i, j - 1, grid, visited, sum + grid[i][j]);
+//         dfs(i, j + 1, grid, visited, sum + grid[i][j]);
+//         visited[i][j] = false;
+//     }
+// }
