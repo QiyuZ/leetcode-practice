25aaf172230aab5ac6fc6eed485c3a11c2eb7f0b
Qiyu Zhang <yuqizhang19@outlook.com>
2020-05-07 23:10:36 -0400

    Update 993. Cousins in Binary Tree.java

--- 993. Cousins in Binary Tree.java
+++ 993. Cousins in Binary Tree.java
@@ -4,29 +4,35 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public boolean isCousins(TreeNode root, int x, int y) {
         Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        boolean a = false, b = false;
-        while(!queue.isEmpty()) {
+        queue.offer(root);//因为在同一层，所以bfs一层层找
+        boolean foundx = false, foundy = false;
+        while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 TreeNode cur = queue.poll();
-                if (cur.val == x) a = true;
-                if (cur.val == y) b = true;
-                if (cur.left != null && cur.right != null) {
+                if (cur.val == x) foundx = true;
+                if (cur.val == y) foundy = true;
+                if (cur.left != null && cur.right != null) { //not same parent
                     if (cur.left.val == x && cur.right.val == y) return false;
-                    if (cur.right.val == x && cur.left.val == y) return false;
+                    if (cur.left.val == y && cur.right.val == x) return false;
                 }
                 if (cur.left != null) queue.offer(cur.left);
                 if (cur.right != null) queue.offer(cur.right);
             }
-            if (a && b) return true;
-            a = false; b = false;
+            if (foundx && foundy) return true; //必须两个有
+            if (foundx || foundy) return false; //因为unique如果只有一个说明不同depth,不成立
         }
         return false;
     }
