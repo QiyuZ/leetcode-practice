e493eccd99e2bf2c466911a95fbb8ffb0098a54c
Qiyu Zhang <yuqizhang19@outlook.com>
2020-05-11 20:36:43 -0400

    Update 733. Flood Fill.java

--- 733. Flood Fill.java
+++ 733. Flood Fill.java
@@ -1,16 +1,18 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-        if (image[sr][sc] == newColor) return image;
-        int origin = image[sr][sc];
-        fill(sr, sc, image, origin, newColor);
+        if (image == null || image.length == 0 || image[sr][sc] == newColor) return image; //newColor就是原来的则不用变
+        int m = image.length, n = image[0].length;
+        dfs(image, sr, sc, newColor, image[sr][sc]);
         return image;
     }
-    public void fill(int sr, int sc, int[][] image, int origin, int newColor) {
-        if (sr < 0 || sr >= image.length || sc < 0 || sc >=image[0].length || image[sr][sc] != origin) return;
-        image[sr][sc] = newColor;
-        fill(sr + 1, sc, image, origin, newColor); //和数岛问题一样
-        fill(sr - 1, sc, image, origin, newColor);
-        fill(sr, sc + 1, image, origin, newColor);
-        fill(sr, sc - 1, image, origin, newColor);
+    private void dfs(int[][] image, int x, int y, int newColor, int origin) { 
+        //可以不用visited因为visited过的一定image[x][y] != origin，第一行已经排除了相等的情况
+        int m = image.length, n = image[0].length;
+        if (x < 0 || x >= m || y < 0 || y >= n || image[x][y] != origin) return;
+        image[x][y] = newColor;
+        dfs(image, x + 1, y, newColor, origin);
+        dfs(image, x - 1, y, newColor, origin);
+        dfs(image, x, y + 1, newColor, origin);
+        dfs(image, x, y - 1, newColor, origin);
     }
 }
