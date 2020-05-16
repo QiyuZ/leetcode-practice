727b49774a8119ff4d684f79bba5ce6498a8617f
Qiyu Zhang <yuqizhang19@outlook.com>
2020-05-16 12:21:24 -0400

    Update 328. Odd Even Linked List.java

--- 328. Odd Even Linked List.java
+++ 328. Odd Even Linked List.java
@@ -13,9 +13,9 @@ class Solution {
         if (head == null) return head;
         ListNode odd = head, even = head.next, evenHead = even;
         while (even != null && even.next != null) {
-            odd.next = even.next;
-            even.next = even.next.next;
+            odd.next = even.next;//交替
             odd = odd.next;
+            even.next = odd.next;
             even = even.next;
         }
         odd.next = evenHead; //连接odd, even
