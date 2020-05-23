ffebfc049b9b5dc1518a1167452bfc3db0a9be68
Qiyu Zhang <yuqizhang19@outlook.com>
2020-05-23 13:51:38 -0400

    Update 986. Interval List Intersections.java

--- 986. Interval List Intersections.java
+++ 986. Interval List Intersections.java
@@ -1,15 +1,15 @@
 class Solution {
     public int[][] intervalIntersection(int[][] A, int[][] B) {
-        int i = 0, j = 0;
         List<int[]> res = new ArrayList<>();
-        while (i < A.length && j < B.length) {
-            int[] a = A[i], b = B[j];
-            int start = Math.max(a[0], b[0]);
-            int end = Math.min(a[1], b[1]);
+        int a = 0, b = 0;
+        while (a < A.length && b < B.length) {//题目中说已经sorted了
+            int[] curA = A[a], curB = B[b];
+            int start = Math.max(curA[0], curB[0]);//找重合开头找大的结尾找小的
+            int end = Math.min(curA[1], curB[1]);
             if (start <= end) res.add(new int[] {start, end});
-            if (a[1] == end) i++; //谁短谁往前走
-            if (b[1] == end) j++;
+            if (curA[1] == end) a++;//哪个在前面就移动哪个，所以可以看取谁的end
+            else b++;
         }
-        return res.toArray(new int[res.size()][2]);
+        return res.toArray(new int[res.size()][2]);//list to array 要标注size大小
     }
 }
