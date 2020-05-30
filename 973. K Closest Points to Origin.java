cb5e38450759b463866d3075935bba6fe2b128e5
Qiyu Zhang <yuqizhang19@outlook.com>
2020-05-30 13:21:34 -0400

    Update 973. K Closest Points to Origin.java

--- 973. K Closest Points to Origin.java
+++ 973. K Closest Points to Origin.java
@@ -1,22 +1,28 @@
+
 class Solution {
-    public int[][] kClosest(int[][] points, int K) {
-        PriorityQueue<int[]> queue = new PriorityQueue<>(K, (a, b) -> ((a[0] * a[0] + a[1] * a[1]) - (b[0] * b[0] + b[1] * b[1])));
-        for (int[] p : points) queue.offer(p);
+    public int[][] kClosest(int[][] points, int K) {  //O(nlogn)
+        Arrays.sort(points, Comparator.comparing(p -> p[0] * p[0] + p[1] * p[1]));
+        return Arrays.copyOfRange(points, 0, K);
+    }
+}
+
+class Solution {
+    public int[][] kClosest(int[][] points, int K) { //O(nlogk)
+        if (points == null || points.length == 0 || K < 1) return new int[][] {};
+        PriorityQueue<int[]> queue = new PriorityQueue<int[]>((a, b) -> (b[1] - a[1]));
+        for (int i = 0; i < points.length; i++) {
+            queue.offer(new int[] {i, points[i][0] * points[i][0] + points[i][1] * points[i][1]});
+            if (queue.size() > K) queue.poll();
+        }
         int[][] res = new int[K][2];
-        int i = 0;
-        while (i < K) {
-            res[i++] = queue.poll();
+        for (int i = 0; i < K; i++) {
+            res[i] = points[queue.poll()[0]];
         }
         return res;
     }
 }
 
-public int[][] kClosest(int[][] points, int K) {
-    Arrays.sort(points, Comparator.comparing(p -> p[0] * p[0] + p[1] * p[1]));
-    return Arrays.copyOfRange(points, 0, K);
-}
-
-class Solution {
+class Solution { //O(n) worst case O(n^2)
     public int[][] kClosest(int[][] points, int K) {
         if (points == null || points.length == 0 || K < 1 || K > points.length) return new int[][] {};
         int l = 0, r = points.length - 1;
