2e9ff886bc4e75ed3262567ac768eee7a530e012
Qiyu Zhang <yuqizhang19@outlook.com>
2020-05-31 12:41:46 -0400

    Update 72. Edit Distance.java

--- 72. Edit Distance.java
+++ 72. Edit Distance.java
@@ -2,15 +2,18 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int m = word1.length(), n = word2.length();
         int[][] dp = new int[m + 1][n + 1];
-        for (int i = 0; i < dp.length; i++) dp[i][0] = i; //初始化，一个是空对于别的都是i
-        for (int i = 0; i < dp[0].length; i++) dp[0][i] = i;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (word1.charAt(i) == word2.charAt(j)) dp[i + 1][j + 1] = dp[i][j];
-                else dp[i + 1][j + 1] = Math.min(dp[i][j + 1], Math.min(dp[i][j], dp[i + 1][j])) + 1;
-                                               //插入（j的插入）        替换       删除（相当于i的插入）
+        for (int i = 0; i <= m; i++) dp[i][0] = i;//注意初始化，因为是要修改的个数一个是0，一个是i所以i个要修改
+        for (int j = 0; j <= n; j++) dp[0][j] = j;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) dp[i][j] = dp[i - 1][j - 1];//本位不修改，结果和上一位一样
+                else dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
+                //本位进行操作base是i-1,j-1，
+                //                       i-1的结果和j一样说明本位要删去，插入    强行和之前一样是替换  
             }
         }
         return dp[m][n];
     }
 }
+
+
