5c179cd44af7a047b303e3ab5bb83dbd36622145
Qiyu Zhang <yuqizhang19@outlook.com>
2020-06-03 21:18:10 -0400

    Update 1029. Two City Scheduling.java

--- 1029. Two City Scheduling.java
+++ 1029. Two City Scheduling.java
@@ -1,9 +1,10 @@
 class Solution {
     public int twoCitySchedCost(int[][] costs) {
-        Arrays.sort(costs, (a, b) -> ((b[1] - b[0]) - (a[1] - a[0]))); //tip 排序差值，以为个数为2N，那么前一半就是costs[i][1]花费少，后一般就是costs[i][0]花费少
+        Arrays.sort(costs, (a, b) -> ((a[0] - a[1]) - (b[0] - b[1]))); //按照差值排序
         int res = 0;
-        for (int i = 0; i < costs.length / 2; i++) {
-            res += costs[i][0] + costs[costs.length - i - 1][1];
+        for (int i = 0; i < costs.length; i++) {
+            if (i < costs.length / 2) res += costs[i][0];//城市相等要一半一半，差值小的选大的这样后面可以选小的
+            else res += costs[i][1];
         }
         return res;
     }
