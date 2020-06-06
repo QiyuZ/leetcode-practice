6d07ca17d99d71f809d39157de1c42e4e8a4ba1c
Qiyu Zhang <yuqizhang19@outlook.com>
2020-06-05 22:23:26 -0400

    Update 528. Random Pick with Weight.java

--- 528. Random Pick with Weight.java
+++ 528. Random Pick with Weight.java
@@ -1,11 +1,13 @@
 class Solution {
 
-    int[]psum;
-    int total = 0;
-    Random rand = new Random();
+    private int[] psum;
+    private int total;
+    private Random rand;
 
-    public Solution(int[] w) {
+    public Solution(int[] w) { //n time to build
         psum = new int[w.length];
+        total = 0;
+        rand = new Random();
         int i = 0;
         for (int x : w) {
             total += x;
@@ -13,7 +15,7 @@ class Solution {
         }
     }
 
-    public int pickIndex() {
+    public int pickIndex() { //logn find time
         int targ = rand.nextInt(total);
         int lo = 0;
         int hi = psum.length - 1;
@@ -26,5 +28,27 @@ class Solution {
             }
         }
         return lo;
+    }
+}
+
+
+class Solution {
+    private TreeMap<Integer, Integer> map;
+    private int sum;
+    private Random random;
+    public Solution(int[] w) { //nlogn build time, because lgn for treemap
+        random = new Random();
+        map = new TreeMap<>();
+        sum = 0;
+        for (int i = 0; i < w.length; i++) {
+            sum += w[i];
+            map.put(sum, i);
+        }
+    }
+    
+    public int pickIndex() {
+        int randNum = random.nextInt(sum) + 1; //nextInt右边不包括所以+1变为[1, sum]
+        int key = map.ceilingKey(randNum);//选取的数如果在就选，不在就找它的上限, logn find time
+        return map.get(key);
     }
 }
