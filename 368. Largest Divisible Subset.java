76c9100cec3f6c9f545925062429143f690ef9f8
Qiyu Zhang <yuqizhang19@outlook.com>
2020-06-13 12:03:45 -0400

    Update 368. Largest Divisible Subset.java

--- 368. Largest Divisible Subset.java
+++ 368. Largest Divisible Subset.java
@@ -1,21 +1,23 @@
 class Solution {
-    private Map<Integer, List<Integer>> map = new HashMap<>();
+    private Map<Integer, List<Integer>> map = new HashMap<>();//keep as memory
     public List<Integer> largestDivisibleSubset(int[] nums) {
-        Arrays.sort(nums);
-        return helper(nums, 0);
+        Arrays.sort(nums);//sort后可以从小开始
+        return findSubset(nums, 0);
     }
-    public List<Integer> helper(int[] nums, int pos) { //记忆化DFS
-        if (map.containsKey(pos)) return map.get(pos);
+    private List<Integer> findSubset(int[] nums, int i) {
+        if (i == nums.length) return new ArrayList<>(); //防止越界
+        if (map.containsKey(i)) return map.get(i);
+        int div = i == 0 ? 1 : nums[i - 1];//如果是起始值可以看做直接加入，否则要除以上一位，因为下面是往后走一位
         List<Integer> res = new ArrayList<>();
-        int div = pos == 0 ? 1 : nums[pos - 1]; //开始第一个数前面没有，用1
-        for (int i = pos; i < nums.length; i++) {
-            if (nums[i] % div == 0) {
-                List<Integer> cur = new ArrayList<>(helper(nums, i + 1)); //可以除尽就接着走并且把当前放进去
-                cur.add(nums[i]);
-                if (cur.size() > res.size()) res = cur; //update
+        for (int j = i; j < nums.length; j++) {
+            if (nums[j] % div == 0) {
+                List<Integer> cur = new ArrayList<>(findSubset(nums, j + 1));
+                //很重要，just copy value，否则会修改reference后面会重复加值，因为前面计算过了
+                cur.add(nums[j]);
+                if (cur.size() > res.size()) res = cur;
             }
         }
-        map.put(pos, res);
+        map.put(i, res);
         return res;
     }
 }
