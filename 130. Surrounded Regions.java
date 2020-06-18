9fb1ed997b8d083ae4ce1cdaa41bee267ac59ae8
Qiyu Zhang <yuqizhang19@outlook.com>
2020-06-17 20:59:11 -0400

    Update 130. Surrounded Regions.java

--- 130. Surrounded Regions.java
+++ 130. Surrounded Regions.java
@@ -2,8 +2,8 @@ class Solution {
     public void solve(char[][] board) {
         if (board == null || board.length == 0) return;
         int m = board.length, n = board[0].length;
-        for (int i = 0; i < n; i++) {
-            if (board[0][i] == 'O') dfs(board, 0, i); //找出连接边缘的，全替换成A，再遍历没有连接边缘的变X
+        for (int i = 0; i < n; i++) {                 //不必每个都找，因为只要连接边缘的就不会变，所以从四个边dfs
+            if (board[0][i] == 'O') dfs(board, 0, i); //找出连接边缘的，全替换成A，这些就是不会被替换的，再遍历没有连接边缘的变X
         }
         for (int i = 0; i < n; i++) {
             if (board[m - 1][i] == 'O') dfs(board, m - 1, i);
@@ -30,5 +30,36 @@ class Solution {
         dfs(board, i - 1, j);
         dfs(board, i, j + 1);
         dfs(board, i, j - 1);
+    }
+}
+
+class Solution { //find all 'O' and use a flag to determine whether replace or not and use list to maintain location
+    private boolean replace = true;
+    private List<int[]> replacedList = new ArrayList<>();
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0) return;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == 'O') {
+                    dfs(board, i, j, new boolean[board.length][board[0].length]);
+                    if (replace) {
+                        for (int[] xy : replacedList) board[xy[0]][xy[1]] = 'X';
+                    }
+                    replace = true;
+                    replacedList = new ArrayList<>();
+                }
+            }
+        }
+    }
+    private void dfs(char[][] board, int i, int j, boolean[][] visited) {
+        int m = board.length, n = board[0].length;
+        if (i < 0 || i == m || j < 0 || j >= n || board[i][j] !='O' || visited[i][j]) return;
+        if (i == 0 || i == m - 1 || j == 0 || j == n - 1) this.replace = false;
+        replacedList.add(new int[]{i, j});
+        visited[i][j] = true;
+        dfs(board, i - 1, j, visited);
+        dfs(board, i + 1, j, visited);
+        dfs(board, i, j - 1, visited);
+        dfs(board, i, j + 1, visited);
     }
 }
