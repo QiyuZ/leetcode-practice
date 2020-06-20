09f0106e1f525a50735079df9aa8b3aa75f783b0
Qiyu Zhang <yuqizhang19@outlook.com>
2020-06-20 13:30:43 -0400

    Update 60. Permutation Sequence.java

--- 60. Permutation Sequence.java
+++ 60. Permutation Sequence.java
@@ -1,19 +1,47 @@
 class Solution {
     public String getPermutation(int n, int k) {
-        List<Integer> map = new ArrayList<>();
-        for (int i = 1; i <= n; i++) map.add(i);
-        int[] factor = new int[n];
+        int[] factor = new int[n]; //i个数可以生成factor[i]个序列比如n=3 [0,1,2]这里不用6因为是不会超过6的
+        List<Integer> nums = new ArrayList<>();
         factor[0] = 1;
-        for (int i = 1; i < n; i++) factor[i] = i * factor[i - 1];
-        k -= 1; //排序是是从1到k，但是代码里是从0到k所以要-1
+        for (int i = 1; i < n; i++) {
+            factor[i] = i * factor[i - 1];
+            nums.add(i);
+        }
+        nums.add(n);
+        k--; //从0开始的，比如1,2,3 k=6其实是找出第5个否则6/2=3而nums里面是0,1,2计数的
         StringBuilder sb = new StringBuilder();
         for (int i = n; i > 0; i--) {
-            int index = k / factor[i - 1]; //最高位的index
-            k %= factor[i - 1];  //相当于 k -= index * factor[i - 1]
-            //比如n=4,k=18(-1=17)的全排列个数是之前的6 再*4，所以前一位确定后=3，那么剩下的就是3个数的排列,就相当于找n=3的第17%6=5位
-            sb.append(map.get(index));
-            map.remove(index); //不能有重复的所以要去掉
+            int index = k / factor[i - 1];//举例如下
+            k -= factor[i - 1] * index;
+            sb.append(nums.get(index));
+            nums.remove(index);
         }
         return sb.toString();
     }
 }
+// example:
+// n = 4, k = 14:
+
+// We have {1, 2, 3, 4}, find the 14th permutation.
+// List out all the permutations:
+
+// 1 + (permutations of 2, 3, 4)
+// 2 + (permutations of 1, 3, 4)
+// 3 + (permutations of 1, 2, 4)
+// 4 + (permutations of 1, 2, 3)
+
+// To find the 14th, we can see it falling to range 3 + (permutations of 1, 2, 4), since 1 + (permutations of 2, 3, 4) and 2 + (permutations of 1, 3, 4) could take the first 2 * (3!) = 12 permutations. 3!即为factor[i - 1]
+//So we can know the first number of result is 3.
+// k = 13 - 2 * 6 = 1 
+// Then the question turn to be a smaller problem.
+// {1, 2, 4}, find the 2nd permutation.
+// List out all the permutations:
+// 1 + (permutations of 2, 4)
+// 2 + (permutations of 1, 4)
+// 4 + (permutations of 1, 2)
+
+// There are 2! + 2! + 2!, 6 permutation. The 2nd must be in range 1 + (permutations of 2, 4). So we can know the second number of result is 1.
+
+// So the question turn be a smaller problem.
+// {2, 4}, find the 2nd permutation. The answer is (4, 2).
+// So the final result is (3, 1, 4, 2)
