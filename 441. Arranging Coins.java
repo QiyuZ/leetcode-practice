c33b3b336e56b12a635a4cb7680dc537d339231f
Qiyu Zhang <yuqizhang19@outlook.com>
2020-07-01 21:47:21 -0400

    Update 441. Arranging Coins.java

--- 441. Arranging Coins.java
+++ 441. Arranging Coins.java
@@ -13,26 +13,17 @@ class Solution {
 }
 
 
-public int arrangeCoins(int n) {   
-        //convert int to long to prevent integer overflow
-        long nLong = (long)n;
-        
-        long st = 0;
-        long ed = nLong;
-        
-        long mid = 0;
-        
-        while (st <= ed){
-            mid = st + (ed - st) / 2;
-            
-            if (mid * (mid + 1) <= 2 * nLong){
-                st = mid + 1;
-            }else{
-                ed = mid - 1;
-            }
+class Solution {
+    public int arrangeCoins(int n) {
+        long l = 1, r = n;
+        while (l < r) {
+            long mid = l + (r - l) / 2;
+            long coins = (1 + mid) * mid / 2;
+            if (coins == n) return (int)mid;
+            else if (coins > n) r = mid;
+            else l = mid + 1;
         }
-        
-        return (int)(st - 1);
+        return (1 + l) * l / 2 > (long)(n) ? (int)(l - 1) : (int)(l);
     }
 }
 
