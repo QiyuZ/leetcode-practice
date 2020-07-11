be93ec5237c58814910c3f900f422e2f7947b5cc
Qiyu Zhang <yuqizhang19@outlook.com>
2020-07-11 12:57:55 -0400

    Update 78. Subsets.java

--- 78. Subsets.java
+++ 78. Subsets.java
@@ -8,6 +8,7 @@ class Solution {
     }
     public void helper(List<List<Integer>> res, int[] nums, int index, List<Integer> list) {
         res.add(new ArrayList<Integer>(list)); //因为是所有子集，所以只要有变化就加进去，包括开头的空集
+        //注意要新建，否则temp backtrack时会变化,要放在开头而不是for里面，因为每次加了新元素都要放进去
         for (int i = index; i < nums.length; i++) {
             list.add(nums[i]);
             helper(res, nums, i + 1, list);
@@ -15,3 +16,4 @@ class Solution {
         }
     }
 }
+//https://leetcode.com/problems/subsets/discuss/27281/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partitioning)
