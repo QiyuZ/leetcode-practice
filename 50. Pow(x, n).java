96321d0b091c7bd33f19aaa07fca95486850c748
Qiyu Zhang <yuqizhang19@outlook.com>
2020-07-16 19:00:15 -0400

    Update 50. Pow(x, n).java

--- 50. Pow(x, n).java
+++ 50. Pow(x, n).java
@@ -1,15 +1,18 @@
 class Solution {
-    public double myPow(double x, int n) {
-        if (n == 0) return 1;
-        long count = n > 0 ? n : -(long)n;
+    public double myPow(double x, int n) { //lgn
+        if (n == 0 || x == 1) return 1;
+        boolean negative = (n < 0);
+        long count = n > 0 ? n : -(long)n; //注意用long,防止Corner case 越界，而且负号要放在long前面，否则结果越界
         double res = 1;
-        while (count != 0) {
-            if (count % 2 == 1) res *= x; //乘上奇数多余的x
-            x *= x;  //偶数个时，先让x自己累乘，后面再一起乘上
-            count /= 2;
+        while (count > 0) {
+            if (count % 2 != 0) { //因为最后肯定是1要乘以前面的x
+                count--;
+                res *= x;
+            } else {
+                count /= 2;
+                x *= x;
+            }
         }
-        return n > 0 ? res : 1.0 / res;
+        return negative ? (1.0 / res) : res;
     }
 }
-
-//log(n)
