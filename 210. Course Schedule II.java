e4f907a8e9a3a0c4db35ca5a0ca91b254fdebd71
Qiyu Zhang <yuqizhang19@outlook.com>
2020-07-18 13:18:02 -0400

    Update 210. Course Schedule II.java

--- 210. Course Schedule II.java
+++ 210. Course Schedule II.java
@@ -1,33 +1,27 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        if (numCourses == 0) return null;
-        int[] indegree = new int[numCourses], res = new int[numCourses];
-        ArrayList<Integer>[] graph = new ArrayList[numCourses]; //构建图
-        int index = 0;
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < numCourses; i++) graph[i] = new ArrayList<Integer>();
+        int[] indegree = new int[numCourses];
+        Map<Integer, Set<Integer>> map = new HashMap<>();
         for (int[] p : prerequisites) {
-            indegree[p[0]]++; //记录需要先修课的课的入度
-            graph[p[1]].add(p[0]); //上了index的课才能上list里面的课
+            indegree[p[0]]++;
+            if (!map.containsKey(p[1])) map.put(p[1], new HashSet<>());
+            map.get(p[1]).add(p[0]);
         }
+        Queue<Integer> queue = new LinkedList<>();
         for (int i = 0; i < indegree.length; i++) {
-            if (indegree[i] == 0) { //说明是起点
-                queue.offer(i);
-                res[index++] = i; //放入结果
-            }
+            if (indegree[i] == 0) queue.offer(i);//不要break因为可能有很多个课没有限制，要把没限制的都加进去
         }
+        int[] res = new int[numCourses];
+        int index = 0; //可能有多重不符合的情况比如整体循环，局部小循环，一个个判断太麻烦，最简单的是记录个数，看符合的是不是最终的numCourses即可
         while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) { //其实这里不用图也可以，直接在prerequisites里面一个个找，再判断入度
-                int cur = queue.poll();
-                for (int classes : graph[cur]) {
-                    if (--indegree[classes] == 0) {
-                        queue.offer(classes);
-                        res[index++] = classes;
-                    }
-                }
+            int cur = queue.poll();
+            res[index++] = cur;
+            if (!map.containsKey(cur)) continue;
+            for (int next : map.get(cur)) {
+                indegree[next]--;
+                if (indegree[next] == 0) queue.offer(next);
             }
         }
-        return index == numCourses ? res : new int[0]; //注意可能有些课冲突无法排序，用index作为判断，不能排序则为空
+        return index == numCourses ? res : new int[] {};
     }
 }
