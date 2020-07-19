cf910b3b015f826bee662906bb94be76d666ec35
Qiyu Zhang <yuqizhang19@outlook.com>
2020-07-19 13:17:11 -0400

    Create 67. Add Binary.java

---
+++ 67. Add Binary.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        if (a == null && b == null) return null;
+        if (a == null || b == null) return a == null ? b : a;
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1, j = b.length() - 1, carry = 0;
+        while (i >= 0 || j >= 0) {
+            int sum = (i >= 0 ? a.charAt(i--) - '0' : 0) + (j >= 0 ? b.charAt(j--) - '0' : 0) + carry;
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        if (carry != 0) sb.append(carry);
+        return sb.reverse().toString();
+    }
+}
