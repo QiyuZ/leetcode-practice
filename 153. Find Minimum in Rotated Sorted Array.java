2254aaa2b102e3c5deefd7ea47a7895ae4a8c784
Qiyu Zhang <yuqizhang19@outlook.com>
2020-07-25 14:02:50 -0400

    Update 153. Find Minimum in Rotated Sorted Array.java

--- 153. Find Minimum in Rotated Sorted Array.java
+++ 153. Find Minimum in Rotated Sorted Array.java
@@ -3,11 +3,11 @@ class Solution {
         if (nums == null || nums.length == 0) return -1;
         int l = 0, r = nums.length - 1;
         if (nums[r] > nums[l]) return nums[l]; //no rotated
-        while (l < r) { //先增后减
+        while (l < r) { //左右两端分别增加但中间有gap
             int mid = l + (r - l) / 2;
-            if (mid > l && nums[mid - 1] > nums[mid]) return nums[mid];//破坏了先增，说明后面那个是target
-            if (mid < r && nums[mid + 1] < nums[mid]) return nums[mid + 1]; //破坏了后减，说明前面的那个是target
-            if (nums[mid] > nums[l]) l = mid + 1;//以pivot为分界线，如果nums[mid] > nums[l]说明mid在左边所以把l右移，反之把r左移
+            if (mid > l && nums[mid - 1] > nums[mid]) return nums[mid];//找到了gap, mid在右边，所以才会nums[mid - 1] > nums[mid]
+            if (mid < r && nums[mid + 1] < nums[mid]) return nums[mid + 1]; //找到了gap, mid在z左边，所以才会nums[mid + 1] < nums[mid]，注意考虑这两种情况，不可以用else if
+            if (nums[mid] > nums[l]) l = mid + 1;//以pivot为分界线，如果nums[mid] > nums[l]说明还在左区间
             else r = mid;
         }
         return nums[l];
