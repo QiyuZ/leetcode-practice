b3d737d8681832258f4375d4fcec4fe2d24fc591
Qiyu Zhang <yuqizhang19@outlook.com>
2020-08-05 20:04:29 -0400

    Update 211. Add and Search Word - Data structure design.java

--- 211. Add and Search Word - Data structure design.java
+++ 211. Add and Search Word - Data structure design.java
@@ -29,6 +29,60 @@ class WordDictionary {
     }
 }
 
+class WordDictionary {  //better way
+    private Trie root, copy;
+    /** Initialize your data structure here. */
+    public WordDictionary() {
+        copy = new Trie();
+        root = copy;
+    }
+    
+    /** Adds a word into the data structure. */
+    public void addWord(String word) {
+        copy = root; //注意add和search都要重置到起点，否则位置都不对
+        for (char w : word.toCharArray()) {
+            if (copy.children[w - 'a'] == null) copy.children[w - 'a'] = new Trie();
+            copy = copy.children[w - 'a'];
+        }
+        copy.word = word;
+    }
+    
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    public boolean search(String word) {
+        copy = root; //每次重置指针的起始点
+        return search(word, copy, 0);
+    }
+    
+    private boolean search(String word, Trie root, int index) {
+        if (index == word.length()) return root.word != null;
+        char cur = word.charAt(index);
+        if (cur == '.') {
+            for (Trie child : root.children) {//查询a-z，有一个符合即可
+                if (child != null && search(word, child, index + 1)) return true;
+            }  
+            return false;//注意如果都不符合直接false
+        } 
+        if (root.children[cur - 'a'] == null) return false;
+        root = root.children[cur - 'a'];
+        return search(word, root, index + 1);
+    }
+    
+    class Trie {
+        private Trie[] children;
+        private String word; //或者直接用boolean isWord
+        public Trie() {
+            this.children = new Trie[26];
+            this.word = null;
+        }
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
 /**
  * Your WordDictionary object will be instantiated and called as such:
  * WordDictionary obj = new WordDictionary();
