8e6b6d7cb71e2b3e15de739347c328ed2ae1d6ac
Qiyu Zhang <yuqizhang19@outlook.com>
2020-08-08 12:10:36 -0400

    Update 270. Closest Binary Search Tree Value.java

--- 270. Closest Binary Search Tree Value.java
+++ 270. Closest Binary Search Tree Value.java
@@ -3,7 +3,7 @@ class Solution {
         int res = root.val;
         while (root != null) {
             if (Math.abs(target - root.val) < Math.abs(target - res)) res = root.val;
-            root = root.val > target ? root.left :root.right;
+            root = root.val > target ? root.left : root.right;
         }
         return res;
     }
@@ -19,5 +19,19 @@ class Solution {
             int b = closestValue(kid, target);
             return Math.abs(a - target) < Math.abs(b - target) ? a : b; //相当于一直两两比较下去
         }
+    }
+}
+
+class Solution {
+    private int res = 0;
+    private double delta = Double.MAX_VALUE;
+    public int closestValue(TreeNode root, double target) {
+        if (root == null) return res;
+        if (Math.abs(target - root.val) < delta) {
+            delta = Math.abs(target - root.val);
+            res = root.val;
+        }
+        if (root.val > target) return closestValue(root.left, target);
+        return closestValue(root.right, target);
     }
 }
