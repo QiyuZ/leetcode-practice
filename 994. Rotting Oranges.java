7632139a78f61cfe86bfd51ec47cae870294dda0
Qiyu Zhang <yuqizhang19@outlook.com>
2020-08-09 13:52:23 -0400

    Update 994. Rotting Oranges.java

--- 994. Rotting Oranges.java
+++ 994. Rotting Oranges.java
@@ -1,30 +1,31 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int fresh = 0;
+        if(grid == null || grid.length == 0) return 0;
+        int fresh = 0, m = grid.length, n = grid[0].length;
         Queue<int[]> queue = new LinkedList<>();
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 1) fresh++; 
-                else if (grid[i][j] == 2) queue.offer(new int[] {i, j});
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) queue.offer(new int[] {i, j});
+                else if (grid[i][j] == 1) fresh++;
             }
         }
-        if (fresh == 0) return 0;
+        int time = 0;
         int[][] dirs = new int[][] {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-        int res = 0;
-        while (!queue.isEmpty()) { 
+        while (!queue.isEmpty()) {
             int size = queue.size();
             for (int count = 0; count < size; count++) {
                 int[] cur = queue.poll();
+                int i = cur[0], j = cur[1];
                 for (int[] d : dirs) {
                     int x = cur[0] + d[0], y = cur[1] + d[1];
-                    if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length || grid[x][y] != 1) continue;
-                    queue.offer(new int[] {x, y});
-                    fresh--;
-                    grid[x][y] = 2;
-                }
+                    if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] != 1) continue;
+                        queue.offer(new int[] {x, y});
+                        fresh--;
+                        grid[x][y] = 2;//注意要在放入时变为2而不是poll时，否则如果两个相连则会重复放入
+                }          
             }
-            res++;
+            if (!queue.isEmpty()) time++;
         }
-        return fresh == 0 ? res - 1 : -1;//bfs一遍看是否可以替换所有的fresh,如果不能就是-1， res-1是因为最后一遍poll出的都是rotten的没有状态转换
+        return fresh == 0 ? time : -1; //可能有触及不到的
     }
 }
