0dfcce01fa28a74a4d32c6e19f18f37ed5dc71a8
Qiyu Zhang <yuqizhang19@outlook.com>
2020-08-23 13:00:26 -0400

    Update 1032. Stream of Characters.java

--- 1032. Stream of Characters.java
+++ 1032. Stream of Characters.java
@@ -1,40 +1,44 @@
 class StreamChecker {
-    private Trie root;
+    
     private StringBuilder sb;
+    private Trie root;
+    
     public StreamChecker(String[] words) {
-        root = new Trie();
         sb = new StringBuilder();
+        root = new Trie();
         buildTrie(words);
     }
     
-    public boolean query(char letter) {
+    public boolean query(char letter) { //query相当于是从后向前看n个字母,所以此处从后往前搜索
         sb.append(letter);
-        Trie node = root;
-        for (int i = sb.length() - 1; i >= 0 && node != null; i--) {
-            node = node.children[sb.charAt(i) - 'a'];  //因为头指针没有字母，所以先走到children再看isWord
-            if (node != null && node.isWord) return true; //要检查新的是不是null因为可能没有该指针
+        Trie copy = root;
+        for (int i = sb.length() - 1; i >= 0; i--) {
+            char c = sb.charAt(i);
+            if (copy.children[c - 'a'] == null) return false;
+            copy = copy.children[c - 'a'];
+            if (copy.isWord) return true;
         }
         return false;
     }
     
-    private void buildTrie(String[] words) { //和query都是倒序
+    private void buildTrie(String[] words) { //建立trie时，也要从后往前，这样才不会被depth影响,比如如果正方向build，abcd和jk深度不同
         for (String word : words) {
-            Trie node = root;
+            Trie copy = root;
             for (int i = word.length() - 1; i >= 0; i--) {
                 char c = word.charAt(i);
-                if (node.children[c - 'a'] == null) node.children[c - 'a'] = new Trie();
-                node = node.children[c - 'a'];
+                if (copy.children[c - 'a'] == null) copy.children[c - 'a'] = new Trie();
+                copy = copy.children[c - 'a'];
             }
-            node.isWord = true;
+            copy.isWord = true;
         }
     }
     
     class Trie {
-        Trie[] children;
-        boolean isWord;
-        public Trie () {
-            children = new Trie[26];
-            isWord = false;
+        private Trie[] children;
+        private boolean isWord;
+        public Trie() {
+            this.children = new Trie[26];
+            this.isWord = false;
         }
     }
 }
