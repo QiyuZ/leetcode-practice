8d7a10a9149e405817e9a44dcd439c0bf5dcc88d
Qiyu Zhang <yuqizhang19@outlook.com>
2020-08-29 21:04:11 -0400

    Update 489. Robot Room Cleaner.java

--- 489. Robot Room Cleaner.java
+++ 489. Robot Room Cleaner.java
@@ -1,30 +1,46 @@
+/**
+ * // This is the robot's control interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface Robot {
+ *     // Returns true if the cell in front is open and robot moves into the cell.
+ *     // Returns false if the cell in front is blocked and robot stays in the current cell.
+ *     public boolean move();
+ *
+ *     // Robot will stay in the same cell after calling turnLeft/turnRight.
+ *     // Each turn will be 90 degrees.
+ *     public void turnLeft();
+ *     public void turnRight();
+ *
+ *     // Clean the current cell.
+ *     public void clean();
+ * }
+ */
+
 class Solution {
-    
-    int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    
+    private static int[][] dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};//题目默认的首先向上，所以这里的顺序是固定的上右下左
     public void cleanRoom(Robot robot) {
-        int[] pos = {0, 0};
-        Set<String> visited = new HashSet<String>();
-        backtracking(robot, pos, visited, 0);
+        backtrack(robot, 0, 0, new HashSet<String>(), 0); //给定起始点0，0 因为不知道map，所以这里的坐标是相对的坐标
     }
     
-    private void backtracking(Robot robot, int[] pos, Set<String> visited, int p) {
-        String position = pos[0] + "," + pos[1];
-        if (visited.contains(position)) return;
-        visited.add(position);
-        robot.clean();
-        for (int i = 0; i < 4; i++) {
-            int newP = (p + i) % 4; //计算相对位置
-            if (!robot.move()) { 
-                robot.turnRight(); 
-                continue; 
+    private void backtrack(Robot robot, int x, int y, Set<String> visited, int index) {
+        String pos = x + "-" + y;
+        visited.add(pos);
+        robot.clean(); //别忘了清理当前点
+        for (int i = 0; i < dirs.length; i++) {
+            int absIndex = (index + i) % 4; 
+            //要取绝对的值，因为每次转身后，方向不同要和dirs能对应，比如说先往上走一步，下次向右时，往前走一步其实是相对起始位置的向右
+            int newX = x + dirs[absIndex][0], newY = y + dirs[absIndex][1];
+            if (!visited.contains(newX + "-" + newY) && robot.move()) {//没走过要可以走
+                backtrack(robot, newX, newY, visited, absIndex);//继续走
+                robot.turnRight();//这两行是180°调头
+                robot.turnRight();
+                robot.move(); //前进一步
+                robot.turnRight(); //这两行是再此调头180°调头，返回完全一样的样子
+                robot.turnRight(); //比如，向上走一步，没路走要回去，就调头走一步,然后再转过头和之前一样
             }
-            int[] newPos = {pos[0] + dirs[newP][0], pos[1] + dirs[newP][1] };
-            backtracking(robot, newPos, visited, newP);
-            robot.turnRight(); //backtracking; move the robot to its original position and facing direction
             robot.turnRight();
-            robot.move();
-            robot.turnLeft(); //转到下一个方向
         }
     }
 }
+
+
