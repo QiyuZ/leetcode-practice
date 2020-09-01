3e85e0b9d13feb63809549ff487cb7783ccf9633
Qiyu Zhang <yuqizhang19@outlook.com>
2020-09-01 19:54:52 -0400

    Update 157. Read N Characters Given Read4.java

--- 157. Read N Characters Given Read4.java
+++ 157. Read N Characters Given Read4.java
@@ -1,18 +1,22 @@
-/* The read4 API is defined in the parent class Reader4.
-      int read4(char[] buf); */
+/**
+ * The read4 API is defined in the parent class Reader4.
+ *     int read4(char[] buf4);
+ */
 
 public class Solution extends Reader4 {
     /**
      * @param buf Destination buffer
-     * @param n   Maximum number of characters to read
-     * @return    The number of characters read
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
      */
     public int read(char[] buf, int n) {
-        int i = 0, len;
-        char[] tmp = new char[4];
-        while (i < n && (len = read4(tmp)) > 0) { //当len=0说明到了尽头
-            for (int j = 0; j < len && i < n; j++, i++) buf[i] = tmp[j];  //多次读，一直往buf里填充，i不重置
+        char[] temp = new char[4];
+        int index = 0;
+        while (index < n) {
+            int count = read4(temp); //题目意思是读取的数字都会放在这个temp里面，同理填充buf
+            if (count == 0) break; //没有了也要停止
+            for (int i = 0; i < count && index < n; i++) buf[index++] = temp[i]; //此处也要index < n防止读空
         }
-        return i;
+        return index;
     }
 }
