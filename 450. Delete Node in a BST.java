5390c8bfb388d35a79af0bb3fc7fee790d8defff
Qiyu Zhang <yuqizhang19@outlook.com>
2020-08-31 21:06:08 -0400

    Update 450. Delete Node in a BST.java

--- 450. Delete Node in a BST.java
+++ 450. Delete Node in a BST.java
@@ -1,22 +1,19 @@
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
         if (root == null) return root;
-        if (key > root.val) root.right = deleteNode(root.right, key);
-        else if (key < root.val) root.left = deleteNode(root.left, key);
-        else {
+        else if (root.val > key) root.left = deleteNode(root.left, key); //注意不直接return的一定要用else
+        else if (root.val < key) root.right = deleteNode(root.right, key);
+        else {                                                           //否则此处，会被多执行一次
             if (root.left == null && root.right == null) return null;
-            if (root.left == null || root.right == null) return root.left == null ? root.right : root.left;
-            root.val = getMin(root.right); //要删除这个点，就要找个替换的，要找右子树的最小或者左子树的最大
-            root.right = deleteNode(root.right, root.val); //替换完以后要删除右边那个最小的
+            else if (root.left == null) return root.right;
+            else if (root.right == null) return root.left;
+            TreeNode temp = root.right;
+            while (temp.left != null) temp = temp.left;
+            int newVal = temp.val; 
+            //如何删除？由于不知道parent node，所以最佳办法是把当前val替换成左最大或者右最小，然后再删除那个左最大或者右最小，依次递归
+            root.val = newVal;
+            root.right = deleteNode(root.right, newVal);
         }
         return root;
-    }
-    public int getMin (TreeNode node) {
-        int min = node.val;
-        while (node.left != null) {
-            node = node.left;
-            min = node.val;
-        }
-        return min;
     }
 }
