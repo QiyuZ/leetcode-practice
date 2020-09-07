8ed9e34082d3e359f3c83b0fd23feb47ec6f0534
Qiyu Zhang <yuqizhang19@outlook.com>
2020-09-06 20:19:18 -0400

    Update 835. Image Overlap.java

--- 835. Image Overlap.java
+++ 835. Image Overlap.java
@@ -1,25 +1,18 @@
 class Solution {
     public int largestOverlap(int[][] A, int[][] B) {
-        List<int[]> a = new ArrayList<>();
-        List<int[]> b = new ArrayList<>();
-        int constant = 1000;
-        int n = A.length;
-        for (int i = 0; i < n * n; i++) {
-            if (A[i / n][i % n] == 1) a.add(new int[] {i / n, i % n});
-            if (B[i / n][i % n] == 1) b.add(new int[] {i / n, i % n});
-        }
-        Map<String, Integer> map = new HashMap<>(); //无论怎么移动相对距离是不变的，所以记录1的位置然后看相对距离相等的有多少个，即为移动后可重合个数
-        for (int[] i : a) {
-            for (int[] j : b) {
-                String key = (i[0] - j[0]) + "-" + (i[1] - j[1]); //特征值法
-                map.put(key, map.getOrDefault(key, 0) + 1);
+        List<Integer> list1 = new ArrayList<>(), list2 = new ArrayList<>();
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[0].length; j++) {
+                if (A[i][j] == 1) list1.add(i * 1000 + j);//根据题目最大length只有30，或者用int[]表示，后面找到x,y坐标差值用string表示key也可以
+                if (B[i][j] == 1) list2.add(i * 1000 + j);
             }
         }
-        int res = 0;
-        for (int num : map.values()) { 
-            if (num > res) res = num;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int a : list1) { //因为不知道具体是怎么移动的，所以把位置的相对差值作为key, 看按照这个移动最多能有几个，即重合的
+            for (int b : list2) {
+                map.put(a - b, map.getOrDefault(a - b, 0) + 1);
+            }
         }
-        return res;
+        return map.isEmpty() ? 0 : Collections.max(map.values());
     }
 }
-
