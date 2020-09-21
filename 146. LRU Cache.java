76c3220f5b984acdb601ba8cc86bd1d33f2c326a
Qiyu Zhang <yuqizhang19@outlook.com>
2020-09-20 22:29:24 -0400

    Update 146. LRU Cache.java

--- 146. LRU Cache.java
+++ 146. LRU Cache.java
@@ -1,64 +1,67 @@
 class LRUCache {
-    
-    private class Node {
-        int key, value;
-        Node pre, next;
-        public Node(int key, int value) {
-            this.key = key; //注意是this.外面的=括号里
-            this.value = value;
-        }
-    }
-    
-    Map<Integer, Node> map;
-    int cap;
-    Node head, tail;
+    private int cap;
+    private Node head, tail; //dummy node,头尾，防止pre或者next是null
+    private Map<Integer, Node> map;
     public LRUCache(int capacity) {
         this.cap = capacity;
-        map = new HashMap<>(capacity);
-        head = new Node(0, 0);
-        tail = new Node(0, 0);
-        head.next = tail; //别忘了相互连接起来
+        this.head = new Node(-1, -1);
+        this.tail = new Node(-1, -1);
+        head.next = tail;
         tail.pre = head;
+        map = new HashMap<>();
     }
     
     public int get(int key) {
-        if (map.containsKey(key)) {
-            Node node = map.get(key);
-            remove(node); //去掉当前的，然后再放到开头
-            insertToHead(node);
-            return node.value;
-        }
-        return -1;
+        if (!map.containsKey(key)) return -1;
+        Node cur = map.get(key);
+        removeNode(cur);
+        setToHead(cur);
+        return cur.value;
     }
     
     public void put(int key, int value) {
-        if (map.containsKey(key)) { //如果已经有了，就更新，并且去掉当前的，然后再放到开头
-            Node cur = map.get(key);
-            remove(cur);
-            insertToHead(cur);
-            cur.value = value;
-            map.put(key, cur);
-        } else {
-            if (map.size() == cap) { //超出cap，移除最后一个，注意head tail其实都是dummy 放删的时候要用head.next或者tail.pre
-                map.remove(tail.pre.key);
-                remove(tail.pre);
-            }
-            Node node = new Node(key, value);
-            insertToHead(node); //要先处理完node后再放入map
-            map.put(key, node);
+        if (map.containsKey(key)) {
+            map.get(key).value = value;//别忘了更新value
+            get(key); //相同操作直接用get
+            return;
+        }
+        if (map.size() == cap) {
+            map.remove(tail.pre.key);//后移除node先map，不然最后一个node已经变了已经变了
+            removeNode(tail.pre);
+        }
+        Node cur = new Node(key, value);
+        setToHead(cur);
+        map.put(key, cur);
+    }  
+    
+    private void removeNode(Node cur) {
+        cur.next.pre = cur.pre;//cur.next.pre之前是cur,现在改成cur.pre, 同理下行
+        cur.pre.next = cur.next;
+    }
+    
+    private void setToHead(Node cur) {
+        Node next = head.next;
+        head.next = cur;
+        cur.pre = head;
+        next.pre = cur;
+        cur.next = next;
+    }
+    
+    private class Node {
+        private int key, value;
+        private Node pre, next;
+        public Node (int key, int value) {
+            this.key = key;
+            this.value = value;
+            this.pre = null;
+            this.next = null;
         }
     }
-    
-    public void remove(Node node) {
-        node.pre.next = node.next;
-        node.next.pre = node.pre;
-    }
-    
-    public void insertToHead(Node node) {
-        Node next = head.next;
-        head.next = node;
-        node.pre = head;
-        node.next = next;
-        next.pre = node;
-    }
 }
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
