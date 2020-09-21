54034f07436afe312d2ef91eff9f50847692476a
Qiyu Zhang <yuqizhang19@outlook.com>
2020-09-20 20:25:01 -0400

    Update 980. Unique Paths III.java

--- 980. Unique Paths III.java
+++ 980. Unique Paths III.java
@@ -1,7 +1,8 @@
 class Solution {
-    private int res = 0, empty = 1, sx, sy, ex, ey;
-    
+    private int count = 0;
     public int uniquePathsIII(int[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
+        int sx = -1, sy = -1, empty = 1;
         int m = grid.length, n = grid[0].length;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
@@ -9,31 +10,29 @@ class Solution {
                 else if (grid[i][j] == 1) {
                     sx = i;
                     sy = j;
-                } else if (grid[i][j] == 2) {
-                    ex = i;
-                    ey = j;
-                }
+                } 
             }
         }
-        dfs(grid, sx, sy);
-        return res;
+        dfs(grid, sx, sy, empty);
+        return count;
     }
     
-    private void dfs(int[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] < 0) return; //-1路障，-2是走过了都不可以
-        if (i == ex && j == ey) { //只要走到end了不管什么情况都要return
-            if (empty == 0) res++;
+    private void dfs(int[][] grid, int i, int j, int empty) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] < 0) return;
+        if (grid[i][j] == 2) { //注意此时一定要return，即使empty不是0，否则line 31要变为之前的值而不是0（考虑2）
+            if (empty == 0) count++;
             return;
-            
         } 
-        int cur = grid[i][j];
-        grid[i][j] = -2; //用-2 标记已经走过了
-        empty--;
-        dfs(grid, i - 1, j);
-        dfs(grid, i + 1, j);
-        dfs(grid, i, j + 1);
-        dfs(grid, i, j - 1);
-         grid[i][j] = cur; //别忘记变回来
-        empty++;
+        grid[i][j] = -1;
+        dfs(grid, i - 1, j, empty - 1);
+        dfs(grid, i + 1, j, empty - 1);
+        dfs(grid, i, j + 1, empty - 1);
+        dfs(grid, i, j - 1, empty - 1);
+        grid[i][j] = 0; //直接变为0即可
     }
 }
+
+
+
+
+
