1b90ce3ecadfa57d2b79c3ddd5ba954099fa46fa
Qiyu Zhang <yuqizhang19@outlook.com>
2020-09-22 19:50:04 -0400

    Update 73. Set Matrix Zeroes.java

--- 73. Set Matrix Zeroes.java
+++ 73. Set Matrix Zeroes.java
@@ -1,28 +1,26 @@
-public class Solution {
+class Solution {
     public void setZeroes(int[][] matrix) {
-        boolean row = false; boolean col = false;
-        for (int i = 0; i < matrix.length; i++) { //判断某个元素是不是0，如果是则吧对应的行或者列放上0（without extra space），其中如果第一行和
-            for (int j = 0; j < matrix[0].length; j++) {  // 和第一列用true false来判断
+        if (matrix == null) return;
+        boolean col = false, row = false; //单独考虑第一行和第一列因为比如第一行被变了对应的列都会变0
+        int m = matrix.length, n = matrix[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 0) {
-                    if (i == 0) row = true;
+                    if (i == 0) row = true; //标记第一行和第一列是否变0
                     if (j == 0) col = true;
+                    matrix[i][0] = 0; //标记的第一行和第一列相应位置
                     matrix[0][j] = 0;
-                    matrix[i][0] = 0;
                 }
             }
         }
-        for(int i = 1; i < matrix.length; i++) {
-            for(int j = 1; j < matrix[0].length; j++) {
-                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    matrix[i][j] = 0;
-                }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) matrix[i][j] = 0; //根据mark决定是否变0
             }
         }
-        if (row) {
-            for (int j = 0; j < matrix[0].length; j++) matrix[0][j] = 0; 
-        }
+        if (row) Arrays.fill(matrix[0], 0); //处理第一行
         if (col) {
-            for (int i = 0; i < matrix.length; i++) matrix[i][0] = 0;
+            for (int i = 0; i < m; i++) matrix[i][0] = 0;//处理第一列
         }
     }
 }
