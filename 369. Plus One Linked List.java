4a719128acbc3bfac54b0b11b3a0adbb1a1d16c6
Qiyu Zhang <yuqizhang19@outlook.com>
2020-09-22 01:26:25 -0400

    Update 369. Plus One Linked List.java

--- 369. Plus One Linked List.java
+++ 369. Plus One Linked List.java
@@ -3,25 +3,26 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode plusOne(ListNode head) {
-        if (head == null) return null;
         ListNode dummy = new ListNode(0);
         dummy.next = head;
-        ListNode cur = dummy, lastNot9 = null;
-        while (cur != null) {
-            if (cur.val != 9) lastNot9 = cur; //找到最后一个位置不是9的比如 199的1， 1293的3，然后这个位置+1，后面的9（如果有）变0
-            cur = cur.next;
+        ListNode copy = dummy, lastNotNine = null; //找出最后一个非9的位置，然后当前+1，后面如果有9都变成0，如果没有则无所谓前面不用管，因为不会进位， 如199找1， 1293找3，123找3
+        while (copy != null) {
+            if (copy.val != 9) lastNotNine = copy;
+            copy = copy.next;
         }
-        lastNot9.val += 1;
-        cur = lastNot9.next;
-        while (cur != null) {
-            cur.val = 0;
-            cur = cur.next;
+        lastNotNine.val += 1;
+        lastNotNine = lastNotNine.next;
+        while (lastNotNine != null && lastNotNine.val == 9) {
+            lastNotNine.val = 0;
+            lastNotNine = lastNotNine.next;
         }
-        return dummy.val == 1 ? dummy : dummy.next;
+        return dummy.val == 0 ? dummy.next : dummy;
     }
 }
