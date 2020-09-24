3fa516067f0f49ad665178a8519a0c25970ed2b0
Qiyu Zhang <yuqizhang19@outlook.com>
2020-09-24 00:43:26 -0400

    Update 362. Design Hit Counter.java

--- 362. Design Hit Counter.java
+++ 362. Design Hit Counter.java
@@ -1,31 +1,64 @@
-// class HitCounter {
+class HitCounter {
 
-//     Queue<Integer> q = null;
-//     /** Initialize your data structure here. */
-//     public HitCounter() {
-//         q = new LinkedList<Integer>();
-//     }
+    Queue<Integer> q = null;
+    /** Initialize your data structure here. */
+    public HitCounter() {
+        q = new LinkedList<Integer>();
+    }
     
-//     /** Record a hit.
-//         @param timestamp - The current timestamp (in seconds granularity). */
-//     public void hit(int timestamp) {
-//         q.offer(timestamp);
-//     }
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public void hit(int timestamp) {
+        q.offer(timestamp);
+    }
     
-//     /** Return the number of hits in the past 5 minutes.
-//         @param timestamp - The current timestamp (in seconds granularity). */
-//     public int getHits(int timestamp) {
-//         while (!q.isEmpty() && timestamp - q.peek() >= 300) {
-//             q.poll();
-//         }
-//         return q.size();
-//     }
-// }
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public int getHits(int timestamp) {
+        while (!q.isEmpty() && timestamp - q.peek() >= 300) {
+            q.poll();
+        }
+        return q.size();
+    }
+}
+
+
+class HitCounter {
+
+    private int[] hits, times;
+    private final static int range = 300;
+    /** Initialize your data structure here. */
+    public HitCounter() {
+        this.hits = new int[range];
+        this.times = new int[range];
+    }
+    
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public void hit(int timestamp) {
+        int index = timestamp % range;
+        if (times[index] != timestamp) { //如果不一样说明要更新，重置hit
+            times[index] = timestamp;
+            hits[index] = 1;
+        } else hits[index]++;//如果是一样的，说明同一时间多次hit则+1
+    }
+    
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public int getHits(int timestamp) {
+        int count = 0;
+        for (int i = 0; i < range; i++) {
+            if (timestamp - times[i] < range) count += hits[i]; //注意此处要用real timestamp来判断范围，count要加hits而不是1
+        }
+        return count;
+    }
+}
+
 
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.concurrent.locks.Lock;
 
-class HitCounter {
+class HitCounter { //thread safe version
     private final ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
     private final Lock r = rwl.readLock();
     private final Lock w = rwl.writeLock();
