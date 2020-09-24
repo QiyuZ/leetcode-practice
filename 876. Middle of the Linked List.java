1af3146d5dbb4038e63794a18aec7fef95a4c9f5
Qiyu Zhang <yuqizhang19@outlook.com>
2020-09-24 01:45:56 -0400

    Update 876. Middle of the Linked List.java

--- 876. Middle of the Linked List.java
+++ 876. Middle of the Linked List.java
@@ -23,13 +23,13 @@ class Solution {
 
 class Solution {
     public ListNode middleNode(ListNode head) {
-        if (head == null) return head;
-        ListNode slow = head, fast = head;
-        while (fast.next != null && fast.next.next != null) {
-            fast = fast.next.next;
+        if(head == null || head.next == null) return head;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
             slow = slow.next;
+            fast = fast.next.next;
         }
-        if (fast.next != null) slow = slow.next; //说明两个middle,取后面的
         return slow;
     }
 }
