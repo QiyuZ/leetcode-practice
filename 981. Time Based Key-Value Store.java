77f268ea191c6398cce41dd8f4a2645c9f93d4ba
Qiyu Zhang <yuqizhang19@outlook.com>
2020-09-28 00:31:20 -0400

    Update 981. Time Based Key-Value Store.java

--- 981. Time Based Key-Value Store.java
+++ 981. Time Based Key-Value Store.java
@@ -1,11 +1,11 @@
-class TimeMap {
+class TimeMap { //both logn for set and get (floorKey(timestamp))
     private Map<String, TreeMap<Integer, String>> map;
     /** Initialize your data structure here. */
     public TimeMap() {
         map = new HashMap<>();
     }
     
-    public void set(String key, String value, int timestamp) {
+    public void set(String key, String value, int timestamp) { 
         if (!map.containsKey(key)) map.put(key, new TreeMap<Integer, String>());
         map.get(key).put(timestamp, value);
     }
@@ -18,6 +18,56 @@ class TimeMap {
     }
 }
 
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
+
+
+
+class TimeMap {
+
+    /** Initialize your data structure here. */
+    Map<String, List<Data>> map;
+    public TimeMap() {
+        map = new HashMap<String, List<Data>>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        if (!map.containsKey(key)) map.put(key, new ArrayList<Data>());
+        map.get(key).add(new Data(value, timestamp));
+    }
+    
+    public String get(String key, int timestamp) {
+        if (!map.containsKey(key)) return "";
+        return binarySearch(map.get(key), timestamp);
+    }
+    
+    protected String binarySearch(List<Data> list, int time) {
+        int low = 0, high = list.size() - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (list.get(mid).time == time) return list.get(mid).val;
+            if (list.get(mid).time < time) {
+                if (list.get(mid + 1).time > time) return list.get(mid).val;
+                low = mid + 1;
+            }
+            else high = mid -1;
+        }
+        return list.get(low).time <= time ? list.get(low).val : "";
+    }
+    
+    private class Data {
+        String val;
+        int time;
+        Data(String val, int time) {
+            this.val = val;
+            this.time = time;
+        }
+    }
+}
 /**
  * Your TimeMap object will be instantiated and called as such:
  * TimeMap obj = new TimeMap();
