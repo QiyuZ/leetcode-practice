3efab0a328036915b9b84862596e0a9df5202ba1
Qiyu Zhang <yuqizhang19@outlook.com>
2020-10-01 22:25:41 -0400

    Update 151. Reverse Words in a String.java

--- 151. Reverse Words in a String.java
+++ 151. Reverse Words in a String.java
@@ -1,12 +1,12 @@
 public class Solution {
     public String reverseWords(String s) {
         if (s == null || s.length() == 0) return s;
-        String[] words = s.split("\\s+"); //可能是多个空格
+        String[] words = s.split("\\s+"); //可能多个空格
         StringBuilder res = new StringBuilder();
         for (int i = words.length - 1; i >= 0; i--) {
-            if (res.length() > 0) res.append(" ");
             res.append(words[i]);
+            res.append(" "); 
         }
-        return res.toString().trim(); //去掉可能有点空格
+        return res.toString().trim();//因为如果前面有多个空格第一个元素会是“”，不一定有没有，所以要trim,防止有
     }
 }
