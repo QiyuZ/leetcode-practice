c2231faaaabd110b5f00a06c166c2e247c15f21e
Qiyu Zhang <yuqizhang19@outlook.com>
2020-10-03 18:01:46 -0400

    Update 641. Design Circular Deque.java

--- 641. Design Circular Deque.java
+++ 641. Design Circular Deque.java
@@ -87,3 +87,85 @@ class MyCircularDeque {
  * boolean param_7 = obj.isEmpty();
  * boolean param_8 = obj.isFull();
  */
+
+
+
+class MyCircularDeque {
+    
+    private int[] arr;
+    int head, tail, size;
+    
+    /** Initialize your data structure here. Set the size of the deque to be k. */
+    public MyCircularDeque(int k) {
+        arr = new int[k];
+        head = -1;
+        tail = 0;
+        size = 0;
+    }
+    
+    /** Adds an item at the front of Deque. Return true if the operation is successful. */
+    public boolean insertFront(int value) {
+        // Overflow
+        if(isFull()) return false;
+        head = (head + 1) % arr.length;
+        // Insert Front
+        arr[head] = value;
+        size++;
+        return true;
+    }
+    
+    /** Adds an item at the rear of Deque. Return true if the operation is successful. */
+    public boolean insertLast(int value) {
+        // Overflow
+        if(isFull()) return false;
+        if(tail == 0) tail = arr.length - 1; //起始状态，放到最后
+        else tail = tail - 1; //否则向前移动
+        // Insert Last
+        arr[tail] = value;
+        size++;
+        return true;
+    }
+    
+    /** Deletes an item from the front of Deque. Return true if the operation is successful. */
+    public boolean deleteFront() {
+        // Overflow
+        if(isEmpty()) return false;
+        if(head == 0) head = arr.length - 1; //回归起始状态，注意此处不是-1，因为可能后面tail还有值
+        else head = head - 1;
+        size--;
+        return true;
+    }
+    
+    /** Deletes an item from the rear of Deque. Return true if the operation is successful. */
+    public boolean deleteLast() {
+        if(isEmpty()) return false;
+        tail = (tail + 1) % arr.length; //注意remove last移动是要向后移,因为最初的放在最后面
+        // Insert Front
+        size--;
+        return true;
+        
+    }
+    
+    /** Get the front item from the deque. */
+    public int getFront() {
+        if(isEmpty()) return -1;
+        return arr[head];
+        
+    }
+    
+    /** Get the last item from the deque. */
+    public int getRear() {
+        if(isEmpty()) return -1;
+        return arr[tail];
+    }
+    
+    /** Checks whether the circular deque is empty or not. */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    
+    /** Checks whether the circular deque is full or not. */
+    public boolean isFull() {
+        return size == arr.length;
+    }
+}
