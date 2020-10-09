148a108c706b700c95a53e7cb241d6b2c503538d
Qiyu Zhang <yuqizhang19@outlook.com>
2020-10-08 22:37:08 -0400

    Update 844. Backspace String Compare.java

--- 844. Backspace String Compare.java
+++ 844. Backspace String Compare.java
@@ -1,28 +1,28 @@
 class Solution {
     public boolean backspaceCompare(String S, String T) {
-        int i = S.length() - 1, j = T.length() - 1;
-        int skip0 = 0, skip1 = 0;
-        while (i >= 0 || j >= 0) { //每次都停到字母上或者到了-1,要包括0因为0也要继续比
+        if (S.equals(T)) return true;
+        int i = S.length() - 1, j = T.length() - 1, skipi = 0, skipj = 0;
+        while (i >= 0 || j >= 0) {
             while (i >= 0) {
                 if (S.charAt(i) == '#') {
+                    skipi++;
+                    i--;  //注意要每个block里面-1
+                } else if (skipi > 0) {
+                    skipi--;
                     i--;
-                    skip0++;
-                } else if (skip0 > 0) {
-                    skip0--;
-                    i--;
-                } else break;
+                } else break; //要break否则出不来
             }
             while (j >= 0) {
                 if (T.charAt(j) == '#') {
+                    skipj++;
                     j--;
-                    skip1++;
-                } else if (skip1 > 0) {
-                    skip1--;
+                } else if (skipj > 0) {
+                    skipj--;
                     j--;
                 } else break;
             }
-            if (i == -1 && j == -1) return true; //min index is -1
-            if (i == -1 || j == -1) return false; 
+            if (i == -1 && j == -1) return true; //最终状态
+            if (i == -1 || j == -1) return false;
             if (S.charAt(i) != T.charAt(j)) return false;
             i--;
             j--;
