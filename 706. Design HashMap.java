6cde1ba9774ae6fffa1a4cebd6f05b3d8bf1b5e0
Qiyu Zhang <yuqizhang19@outlook.com>
2020-10-10 13:51:14 -0400

    Update 706. Design HashMap.java

--- 706. Design HashMap.java
+++ 706. Design HashMap.java
@@ -31,53 +31,67 @@ class MyHashMap {
 }
 
 
- class MyHashMap {
-        final ListNode[] nodes = new ListNode[10000];
+class MyHashMap {
 
-        public void put(int key, int value) {
-            int i = idx(key);
-            if (nodes[i] == null)
-                nodes[i] = new ListNode(-1, -1);
-            ListNode prev = find(nodes[i], key);
-            if (prev.next == null)
-                prev.next = new ListNode(key, value);
-            else prev.next.val = value;
+    private static final int size = 1000;
+    private Node[] nodes;
+    /** Initialize your data structure here. */
+    public MyHashMap() {
+        nodes = new Node[size]; //使用array防止same hash index
+    }
+    
+    /** value will always be non-negative. */
+    public void put(int key, int value) {
+        int index  = getIndex(key);
+        Node node = getNode(index, key);
+        if (node.next == null) node.next = new Node(key, value);
+        else node.next.value = value;
+        
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    public int get(int key) {
+        int index  = getIndex(key);
+        Node node = getNode(index, key);
+        if (node.next == null) return -1;
+        return node.next.value;
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    public void remove(int key) {
+        int index  = getIndex(key);
+        Node node = getNode(index, key);
+        if (node.next == null) return;
+        node.next = node.next.next;
+    }
+    
+    private int getIndex(int key) {
+        return Integer.hashCode(key) % size; //注意要%size防止越界
+    }
+    
+    private Node getNode(int index, int key) {
+        if (nodes[index] == null) {  //注意这个时候不能直接取node赋值，因为这个时候是null，要直接初始化这个Node
+            nodes[index] = new Node(-1, -1);
+            return nodes[index];
         }
-
-        public int get(int key) {
-            int i = idx(key);
-            if (nodes[i] == null)
-                return -1;
-            ListNode node = find(nodes[i], key);
-            return node.next == null ? -1 : node.next.val;
-        }
-
-        public void remove(int key) {
-            int i = idx(key);
-            if (nodes[i] == null) return;
-            ListNode prev = find(nodes[i], key);
-            if (prev.next == null) return;
-            prev.next = prev.next.next;
-        }
-
-        int idx(int key) { return Integer.hashCode(key) % nodes.length;}
-
-        ListNode find(ListNode bucket, int key) {
-            ListNode node = bucket, prev = null;
-            while (node != null && node.key != key) {
-                prev = node;
-                node = node.next;
-            }
-            return prev;
-        }
-
-        class ListNode {
-            int key, val;
-            ListNode next;
-
-            ListNode(int key, int val) {
-                this.key = key;
-                this.val = val;
-            }
+        Node node = nodes[index];
+        while (node.next != null && node.next.key != key) node = node.next;
+        return node;
+    }    
+    
+    class Node {
+        private int key, value;
+        private Node next;
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
         }
     }
+}
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
