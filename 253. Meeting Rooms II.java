d0b4d8c3fcaf94244d3a6d46d54acc9f373afc6b
Qiyu Zhang <yuqizhang19@outlook.com>
2020-10-15 19:54:08 -0400

    Update 253. Meeting Rooms II.java

--- 253. Meeting Rooms II.java
+++ 253. Meeting Rooms II.java
@@ -1,23 +1,13 @@
-/**
- * Definition for an interval.
- * public class Interval {
- *     int start;
- *     int end;
- *     Interval() { start = 0; end = 0; }
- *     Interval(int s, int e) { start = s; end = e; }
- * }
- */
 class Solution {
-    public int minMeetingRooms(Interval[] intervals) {
-        if (intervals == null || intervals.length == 0) return 0;
-        Arrays.sort(intervals, (a, b) -> {return a.start - b.start;});
-        PriorityQueue<Interval> queue = new PriorityQueue<>((a, b) -> {return a.end - b.end;});
-        queue.offer(intervals[0]);
-        for (int i = 1; i < intervals.length; i++) {
-            Interval cur = queue.poll();
-            if (cur.end <= intervals[i].start) cur.end = intervals[i].end; //可以合并的
-            else queue.offer(intervals[i]); //不能合并的加进去
-            queue.offer(cur); //之前去除的放回去
+    public int minMeetingRooms(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> (Integer.compare(a[0], b[0])));
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> (Integer.compare(a[1], b[1])));
+        for (int[] interval : intervals) {
+            while (!queue.isEmpty() && queue.peek()[1] <= interval[0]) { 
+                int[] pre = queue.poll();
+                interval[0] = Math.min(interval[0], pre[0]);//注意这里面不能offer进queue因为可能后面还有
+            }
+            queue.offer(interval); //要到最后无论有没有poll当前的才能进
         }
         return queue.size();
     }
