fe59b2c648b71f20a3abfe32e127921306ed2879
Qiyu Zhang <yuqizhang19@outlook.com>
2020-10-15 00:08:05 -0400

    Update 498. Diagonal Traverse.java

--- 498. Diagonal Traverse.java
+++ 498. Diagonal Traverse.java
@@ -1,25 +1,27 @@
 class Solution {
     public int[] findDiagonalOrder(int[][] matrix) {
-        if(matrix == null || matrix.length == 0) return new int[0];
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return new int[] {};
         int m = matrix.length, n = matrix[0].length;
         int[] res = new int[m * n];
-        int r = 0, c = 0;
-        for (int i = 0; i < res.length; i++) {
-            res[i] = matrix[r][c];
-            if ((r + c) % 2 == 0) { //往上移动
-                if (c == n - 1) r++; //特殊走到头，右边没了项=向下移动
-                else if (r == 0) c++; //一般走到头，向右
-                else {r--; c++;} //一般就直接右上方
-            } else {
-                if (r == m - 1) c++;//一般走到头，右移
-                else if (c == 0) r++;//特殊走到头，左边没了向下
-                else {r++; c--;} //一般情况，直接左下移动
+        int r = 0, c = 0, i = 0;
+        while (i < res.length) {
+            res[i++] = matrix[r][c];
+            if ((r + c) % 2 == 0) { //根据方向分为两组，左上的坐标都是2的倍数
+                if (c == n - 1) r++; //一般来说走到左上的头后要向右移，但是如果右边没有了就要像下移
+                else if (r == 0) c++;//一般向右移
+                else {
+                    r--;
+                    c++;
+                }
+            } else { ///根据方向分为两组，右下的坐标都不是2的倍数
+                if (r == m - 1) c++; //一般来说走到右下的头后要向下移，但是如果右边没有了就要像右移
+                else if (c == 0) r++;//一般来说走到右下的头后要向下移
+                else {
+                    r++;
+                    c--;
+                }
             }
         }
         return res;
     }
 }
-
-/*case1. 左下或者右上直接移动
-case2. 一般走到头，向右移
-特殊走到头（左/右边没了）向下移动*/
