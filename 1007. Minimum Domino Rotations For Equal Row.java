18db565717642824ebb365406b67e7690e2883ab
Qiyu Zhang <yuqizhang19@outlook.com>
2020-10-19 19:51:24 -0400

    Update 1007. Minimum Domino Rotations For Equal Row.java

--- 1007. Minimum Domino Rotations For Equal Row.java
+++ 1007. Minimum Domino Rotations For Equal Row.java
@@ -1,31 +1,31 @@
 class Solution {
     public int minDominoRotations(int[] A, int[] B) {
-        int[] count = new int[7];
-        for (int a : A) count[a]++;
-        for (int b : B) count[b]++;
-        int max = 0, time = 0;
-        for (int i = 0; i < 7; i++) {
-            if (count[i] > time) {
-                time = count[i];
-                max = i;
-            }
+        int[] markA = new int[7], markB = new int[7];
+        int n = A.length;
+        for (int i = 0; i < n; i++) {
+            markA[A[i]]++;
+            markB[B[i]]++;
         }
-        int swapA = 0, swapB = 0;
-        if (time < A.length) return -1;
-        for (int i = 0; i < A.length; i++) {
-            if (A[i] == B[i]) {
-                if (A[i] == max) continue;//两者都等于
-                else return -1; //都不等于这个位置就换不了
-            } else if (A[i] == max)  swapB++; //如果换到B
-            else if (B[i] == max)  swapA++; //如果换到A
-            else return -1;
+        for (int i = 1; i <= 6; i++) {
+            if (markA[i] == n || markB[i] == n) return 0;
+            if (markA[i] + markB[i] >= n) return getCount(i, A, B); 
+            //只会有一个或者两个结果，因为长度是2n,而如果有两个则答案是相同的,都成立或者都不成立
         }
-        return Math.min(swapA, swapB);
+        return -1;
+    }
+    
+    private int getCount(int target, int[] A, int[] B) {
+        int n = A.length, countA = 0, countB = 0, swapA = 0, swapB = 0;
+        for (int i = 0; i < n; i++) {
+            if (target == A[i] && target != B[i]) swapB++;
+            else if (target != A[i] && target == B[i]) swapA++;
+            if (target == A[i]) countA++;
+            if (target == B[i]) countB++;
+        }
+        if (countA + swapA == n && countB + swapB == n) return Math.min(swapA, swapB);
+        if (countA + swapA == n) return swapA;
+        if (countB + swapB == n) return swapB;
+        return -1;
+        
     }
 }
-/*
-其实只需要考虑出现次数最多的数字
-case1： 出现最多的小于A长度，那么-1因为其他的也都不可以满足
-case2:  等于长度，只能是它，看如何交换最小
-case3： 大于长度也之只能是它，因为此种情况下出现次数第二大的肯定小于A.length, 处理方式同case2如何交换
-*/
