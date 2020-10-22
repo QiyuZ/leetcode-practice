9b71cddd9722e592d94505b4f9da923e88024c06
Qiyu Zhang <yuqizhang19@outlook.com>
2020-10-21 23:34:20 -0400

    Update 735. Asteroid Collision.java

--- 735. Asteroid Collision.java
+++ 735. Asteroid Collision.java
@@ -1,30 +1,18 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        if (asteroids == null || asteroids.length == 0) return asteroids;
         Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < asteroids.length; i++) {
-            if (stack.isEmpty() || asteroids[i] > 0) { //空地方或者正数，直接加入，注意接下来asteroids[i]是<0了
-                stack.push(asteroids[i]);
-                continue;
-            }
-            while (true) {
-                int pre = stack.peek();
-                if (pre < 0) {  //符号相同，直接加入，注意接下来都是pre>0了
-                    stack.push(asteroids[i]);
-                    break;
-                } else if (pre == -asteroids[i]) { //相等都爆炸
-                    stack.pop();
-                    break;
-                } else if (pre > -asteroids[i]) break; //原来的大，新来的爆炸没操作
-                else stack.pop(); //剩下的情况都是 新来的大，大的爆炸，因为还要接着和下一个peek比较，所以先不加入
-                if (stack.isEmpty()) { //如果已经空了，直接加入
-                    stack.push(asteroids[i]);
-                    break;
-                }
+        for(int i : asteroids) {
+            if(i > 0) stack.push(i);
+            else{
+                while(!stack.isEmpty() && stack.peek() > 0 && stack.peek() < -i) stack.pop();
+                //这个大上个炸，注意用while因为大的可以一直保持去炸小的
+                if (!stack.isEmpty() && stack.peek() > -i) continue; //本个炸
+                else if(!stack.isEmpty() && stack.peek() == -i) stack.pop();//两都炸，不同push
+                else stack.push(i);
             }
         }
         int[] res = new int[stack.size()];
-        for (int i = stack.size() - 1; i >= 0; i--) res[i] = stack.pop();
+        for(int i = res.length - 1; i >= 0; i--) res[i] = stack.pop();
         return res;
     }
 }
