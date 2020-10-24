b34c8f456767795ab8726ff624e28b911584e487
Qiyu Zhang <yuqizhang19@outlook.com>
2020-10-24 13:00:08 -0400

    Update 948. Bag of Tokens.java

--- 948. Bag of Tokens.java
+++ 948. Bag of Tokens.java
@@ -1,17 +1,15 @@
 class Solution {
-    public int bagOfTokensScore(int[] tokens, int P) { //greedy，牺牲积分换大power，获得积分用较少的power
-        if (tokens.length == 0) return 0;
-        int left = 0, right = tokens.length - 1, points = 0, res = 0;
+    public int bagOfTokensScore(int[] tokens, int P) {
+        int l = 0, r = tokens.length - 1, score = 0;
         Arrays.sort(tokens);
-        while (left <= right) {
-            if (P >= tokens[left]) { //左边在前面因为当left=right，游戏要结束优先考虑得分
-                P -= tokens[left++];
-                res = Math.max(res, ++points);
-            } else if (points > 0) {
-                P += tokens[right--];
-                points--;
-            } else break;
+        while (l <= r) {
+            if (P >= tokens[l]) { //能买的情况下优先买，增加score因为这是最终结果
+                score++;
+                P -= tokens[l++];
+            } else if (score > 0 && tokens[r] > tokens[l]) P = P + tokens[r--] - tokens[l++];
+            //如果不够买了，那么就用score换再买前面的，前提是power得是增加的即tokens[r] > tokens[l]，不然是亏的
+            else break; //注意这一行是必要的因为没有cover所有情况，剩下的情况说明即买不了也换不了，退出
         }
-        return res;
+        return score;
     }
 }
