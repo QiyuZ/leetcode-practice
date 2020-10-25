0d264fa53b61e3a4b495f366e3cd290187d6513a
Qiyu Zhang <yuqizhang19@outlook.com>
2020-10-25 14:54:40 -0400

    Update 958. Check Completeness of a Binary Tree.java

--- 958. Check Completeness of a Binary Tree.java
+++ 958. Check Completeness of a Binary Tree.java
@@ -26,3 +26,17 @@ class Solution {
 }
 
 
+class Solution {
+    public boolean isCompleteTree(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+            if (cur == null) break; //如果是CompleteTree，那么只有最后一个才是null,否则break后queue还有tree说明null在中间即不满足
+            queue.offer(cur.left);
+            queue.offer(cur.right);
+        }
+        while (!queue.isEmpty() && queue.peek() == null) queue.poll();//注意此处后面可能有多个null要全poll出后看有没有valid node在后面
+        return queue.isEmpty();
+    }
+}
