d608988852bd06a45f6ee58424a5e3778f89e027
Qiyu Zhang <yuqizhang19@outlook.com>
2020-10-26 18:51:21 -0400

    Update 799. Champagne Tower.java

--- 799. Champagne Tower.java
+++ 799. Champagne Tower.java
@@ -1,15 +1,18 @@
 class Solution {
     public double champagneTower(int poured, int query_row, int query_glass) {
-        double[][] res = new double[query_row + 1][query_row + 1];
-        res[0][0] = poured;
+        double[][] glasses = new double[query_row + 1][query_row + 1]; 
+        //此处注意第二个也是query_row + 1表明一行中最多的个数，否则如果是个query_glass是个特别小的数那么前几行后面无法计算
+        if (poured <= 0) return 0.0;
+        glasses[0][0] = poured; //初始化
         for (int i = 0; i < query_row; i++) {
-            for (int j = 0; j <= i; j++) {
-                double remain = Math.max(res[i][j] - 1.0, 0); //每次上面有剩余的就往下平均分成两份
-                if (remain == 0) continue;
-                res[i + 1][j] += remain / 2;
-                res[i + 1][j + 1] += remain / 2;
+            for (int j = 0; j <= i; j++) { //注意此处是<=i因为当前的酒只能留到下一行两侧，eg. (1,0) -> (2,0), (2,1)
+                double remain = glasses[i][j] - 1.0;
+                if (remain > 0) {
+                    glasses[i + 1][j] += remain / 2;
+                    glasses[i + 1][j + 1] += remain / 2; 
+                }
             }
         }
-        return Math.min(res[query_row][query_glass], 1.0);
+        return Math.min(glasses[query_row][query_glass], 1.0); //注意可能有剩余的，取1.0
     }
 }
