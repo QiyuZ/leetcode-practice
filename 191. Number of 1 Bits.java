7c9340f73a16dfd9d50cf58b16d2798756e2dbd3
Qiyu Zhang <yuqizhang19@outlook.com>
2020-10-25 22:21:12 -0400

    Update 191. Number of 1 Bits.java

--- 191. Number of 1 Bits.java
+++ 191. Number of 1 Bits.java
@@ -2,9 +2,7 @@ public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
         int count = 0;
-        for (int i = 0; i <= 31; i++) {
-            count += ((n >> i) & 1);
-        }
+        for (int i = 0; i < 32; i++) count += (n >> i) & 1;//1011&0001 -> 101&001 -> 10&01 -> 1&1注意是向右移从后往前比较，而1只有最后一位有1所以比较最后一位
         return count;
     }
 }
