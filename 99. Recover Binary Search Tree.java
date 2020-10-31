c4bc2111eaa9b55f2886fad0c602ddf490c9adfc
Qiyu Zhang <yuqizhang19@outlook.com>
2020-10-31 12:06:49 -0400

    Update 99. Recover Binary Search Tree.java

--- 99. Recover Binary Search Tree.java
+++ 99. Recover Binary Search Tree.java
@@ -4,38 +4,41 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    TreeNode first = null, second = null, pre = new TreeNode(Integer.MIN_VALUE);
+    private TreeNode first = null, second = null, pre = null;
     public void recoverTree(TreeNode root) {
-        inorderTraverse(root);
-        swap(first, second);
-    }
-    
-    public void inorderTraverse(TreeNode root) {
-        if(root == null) return;
-        inorderTraverse(root.left);
-        //first wrong value will be bigger than current so first element = prev
-        if(first == null && pre.val > root.val) first = pre;
-        //second wrong value will be smaller than current so second element = root
-        if(first != null && pre.val > root.val) second = root; 
-        pre = root;
-        inorderTraverse(root.right);
-    }
-    
-    private void swap(TreeNode first, TreeNode second) {
+        findTwo(root);
         int temp = first.val;
         first.val = second.val;
         second.val = temp;
     }
+    private void findTwo(TreeNode root) {
+        if (root == null) return;
+        findTwo(root.left);
+        if (pre != null && pre.val > root.val && first == null) first = pre;//要两个if都并列
+        if (pre != null && pre.val > root.val && first != null) second = root;
+        //注意此处是找前面的大的和后面的小的交换，因为大的本来就要在后面不可以再换到前面
+        //所以对于first找大的即pre，second找小的即root
+        pre = root;
+        findTwo(root.right);
+    }
 }
+
 /*
-不可以写成
+注意不可以写成
 if(pre.val > root.val) {
     if (first == null) first = pre;
-    else second = pre;
+    else second = root;
 }
 这两个要同时因为可能并列 1274564 一个if就行，但是如果132这个时候就找不到2了
+first 取前面的 second取后面的，因为找大的放在后面，找小的放在前面
 */
