0e14f9181d8123ac230b420de958bb0b6ac6c9dd
Qiyu Zhang <yuqizhang19@outlook.com>
2020-10-31 12:09:43 -0400

    Update 253. Meeting Rooms II.java

--- 253. Meeting Rooms II.java
+++ 253. Meeting Rooms II.java
@@ -3,11 +3,8 @@ class Solution {
         Arrays.sort(intervals, (a, b) -> (Integer.compare(a[0], b[0])));
         PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> (Integer.compare(a[1], b[1])));
         for (int[] interval : intervals) {
-            while (!queue.isEmpty() && queue.peek()[1] <= interval[0]) { 
-                int[] pre = queue.poll();
-                interval[0] = Math.min(interval[0], pre[0]);//注意这里面不能offer进queue因为可能后面还有
-            }
-            queue.offer(interval); //要到最后无论有没有poll当前的才能进
+            if (!queue.isEmpty() && queue.peek()[1] <= interval[0]) queue.poll();
+            queue.offer(interval);
         }
         return queue.size();
     }
