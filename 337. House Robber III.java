fce194c22b83a5aeaf968d04721e2605e1a0c310
Qiyu Zhang <yuqizhang19@outlook.com>
2021-12-05 16:06:27 -0500

    Update 337. House Robber III.java

--- 337. House Robber III.java
+++ 337. House Robber III.java
@@ -4,21 +4,29 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public int rob(TreeNode root) {
-        int[] res = dfs(root);
+        if (root == null) return 0;
+        int[] res = robWithTwoCases(root);
         return Math.max(res[0], res[1]);
     }
-    public int[] dfs(TreeNode root) {
-        if (root == null) return new int[2];
-        int[] left = dfs(root.left);
-        int[] right = dfs(root.right);
+    
+    private int[] robWithTwoCases(TreeNode root) {
+        if (root == null) return new int[] {0, 0};
         int[] res = new int[2];
-        res[0] = left[1] + right[1] + root.val; //0为要rob当前，那么要加当前的和左右两边相邻没有rob的
-        res[1] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]); //1表示不rob当前的，那么左右两边有没有rob没关系，所以找最大的
+        int[] left = robWithTwoCases(root.left), right = robWithTwoCases(root.right);
+        res[0] = root.val + left[1] + right[1]; //index 0 alaways means rob current one, then we cant rob left and right, so we can only use left[1] and right[1]
+        res[1] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        //index 1 means not rob current one, then for the left or right, note that it doesnt mean we must use left[0] or right[0], because we cant really tell it's that's the best case, so we just compare and find the larget one
         return res;
     }
 }
