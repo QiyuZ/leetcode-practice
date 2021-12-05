23e9197c8b749a068416ff8182725c5914470b9a
Qiyu Zhang <qz5uw@virginia.edu>
2021-12-05 16:29:48 -0500

    Create 2089. Find Target Indices After Sorting Array.java

---
+++ 2089. Find Target Indices After Sorting Array.java
@@ -0,0 +1,28 @@
+//nlog(n)
+class Solution {
+    public List<Integer> targetIndices(int[] nums, int target) {
+        List<Integer> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) res.add(i);
+        }
+        return res;
+    }
+}
+
+//O(n)
+class Solution {
+    public List<Integer> targetIndices(int[] nums, int target) {
+        int count = 0, lessthan = 0;
+        for (int n : nums) {
+            if (n == target) count++;
+            if (n < target) lessthan++;
+        }
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            result.add(lessthan++);
+        }
+        return result;
+    }
+}
+
