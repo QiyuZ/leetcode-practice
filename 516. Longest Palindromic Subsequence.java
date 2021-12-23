b1e44a6af435e6e9b7862f6752a2d61d9f0c3060
Qiyu Zhang <qz5uw@virginia.edu>
2021-12-23 00:49:27 -0500

    Update 516. Longest Palindromic Subsequence.java

--- 516. Longest Palindromic Subsequence.java
+++ 516. Longest Palindromic Subsequence.java
@@ -16,3 +16,18 @@ class Solution {
 /*举例 bbbab dp[0][4] 两b相等，则看bba有几个回文。2个，加2
 cbbd dp[1][2] 两b相等 dp[2][1]不存在,但是不影响，因为是0
      dp[0][3] 不相等，则看bbd或者cbb最大值 */
+
+class Solution {
+    public int longestPalindromeSubseq(String s) { //refer 1143. Longest Common Subsequence
+        if (s == null || s.length() <= 1) return 1;
+        int n = s.length();
+        int[][] dp = new int[n + 1][n + 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (s.charAt(i) == s.charAt(n - 1 - j)) dp[i + 1][j + 1] = dp[i][j] + 1; //kind like compare the string with its reversed one
+                else dp[i + 1][j + 1] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+            }
+        }
+        return dp[n][n];
+    }
+}
