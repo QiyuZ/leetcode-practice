c8b582c866253a6d5efe9f63b294b1f6b99a8322
Qiyu Zhang <qz5uw@virginia.edu>
2021-12-24 14:00:47 -0500

    Create 1570. Dot Product of Two Sparse Vectors.java

---
+++ 1570. Dot Product of Two Sparse Vectors.java
@@ -0,0 +1,45 @@
+class SparseVector {  //write heavy 
+    private int[] nums;
+    SparseVector(int[] nums) {
+        this.nums = nums;
+    }
+    
+	// Return the dotProduct of two sparse vectors
+    public int dotProduct(SparseVector vec) {
+        int[] other = vec.nums;
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) res += nums[i] * other[i];
+        return res;
+    }
+}
+
+// Your SparseVector object will be instantiated and called as such:
+// SparseVector v1 = new SparseVector(nums1);
+// SparseVector v2 = new SparseVector(nums2);
+// int ans = v1.dotProduct(v2);
+class SparseVector { //read heavy
+    Map<Integer, Integer> map;      // For all non-zero values in the vector, we map the index to the non-zero value.
+    
+    // O(nums.length) time.
+    // O(numNonZeroValues) space.
+    SparseVector(int[] nums) {
+        map = new HashMap<>();
+        // Store the position and value of non-zero values.
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] != 0) map.put(i, nums[i]);        
+        }
+    }
+
+
+    // O(min(vec1numNonZeroValues, vec2numNonZeroValues)) time because we iterate through non-zero values of the vector that has fewer non-zero values and for each value we check in O(1) time whether the other vector has a non-zero value at that index.
+    // O(1) space.
+    public int dotProduct(SparseVector vec) {        
+        if (vec.map.size() < this.map.size()) return vec.dotProduct(this);      // We want to iterate through the smaller map.
+        int result = 0;
+        for (int currIdx : this.map.keySet()) {
+            // If both vectors have a non-zero value at currIdx then multiply the values and add them to the result.
+            if (vec.map.containsKey(currIdx)) result += this.map.get(currIdx) * vec.map.get(currIdx);
+        }
+        return result;
+    }
+}
