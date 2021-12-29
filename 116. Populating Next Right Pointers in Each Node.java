d86416c161b88800a5736cafd3b9e1d4efe3c1e6
Qiyu Zhang <qz5uw@virginia.edu>
2021-12-29 16:00:25 -0500

    Update 116. Populating Next Right Pointers in Each Node.java

--- 116. Populating Next Right Pointers in Each Node.java
+++ 116. Populating Next Right Pointers in Each Node.java
@@ -9,3 +9,20 @@ public class Solution { //这个保证两个children,117不保证
         connect(root.right);
     }
 }
+
+class Solution { //level traversal without extra space
+    public Node connect(Node root) {
+        if (root == null) return root;
+        Node leftMost = root;
+        while (leftMost.left != null) {
+            Node head = leftMost;
+            while (head != null) {
+                if (head.left != null) head.left.next = head.right;
+                if (head.next != null && head.right != null) head.right.next = head.next.left;
+                head = head.next;
+            }
+            leftMost = leftMost.left;
+        }
+        return root;
+    }
+}
