086c231393d3bcc9dcbe81683d4438dea2003bec
Qiyu Zhang <qz5uw@virginia.edu>
2021-12-30 15:47:03 -0500

    Update 1015. Smallest Integer Divisible by K.java

--- 1015. Smallest Integer Divisible by K.java
+++ 1015. Smallest Integer Divisible by K.java
@@ -1,10 +1,11 @@
 class Solution {
-	public int smallestRepunitDivByK(int K) {
-        if (K % 2 == 0 || K % 5 == 0) return -1; 
-		for(int i=1,val=1; i<=K; i++){ //到K即可，必有重复
-			if(val % K == 0) return i;
-			val = (val * 10 + 1) % K; //(a+b)%k =(a%k+b%k)%k，所以相当于算里面的即可
-		}    
-		return -1;
-	}
+    public int smallestRepunitDivByK(int k) {
+		if (k % 2 == 0 || k % 5 == 0) return -1;  // this trick may save a little time
+        int n = 0;
+        for (int i = 0; i < k; i ++) { // at most k times, because 0 <= remainder < k
+            n = (n * 10 + 1) % k; // we only focus on whether to divide, so we only need to keep the remainder.
+            if (n == 0) return i + 1; // i started from 0, so before return it should be added 1
+        }
+        return -1; // If it had not stop after k transfers, it must enter the loop, and it won't stop.
+    }
 }
