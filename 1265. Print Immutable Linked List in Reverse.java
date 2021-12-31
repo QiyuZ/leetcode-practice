b25cf45255c83865ad642a171331f8ce19962355
Qiyu Zhang <qz5uw@virginia.edu>
2021-12-30 19:24:12 -0500

    Create 1265. Print Immutable Linked List in Reverse.java

---
+++ 1265. Print Immutable Linked List in Reverse.java
@@ -0,0 +1,18 @@
+/**
+ * // This is the ImmutableListNode's API interface.
+ * // You should not implement it, or speculate about its implementation.
+ * interface ImmutableListNode {
+ *     public void printValue(); // print the value of this node.
+ *     public ImmutableListNode getNext(); // return the next node.
+ * };
+ */
+
+class Solution { //O(n)
+    public void printLinkedListInReverse(ImmutableListNode head) {
+        if (head == null) return;
+        printLinkedListInReverse(head.getNext());
+        head.printValue();
+    }
+}
+
+//O(n^2) but constant space, get count first and then get next every time to print
