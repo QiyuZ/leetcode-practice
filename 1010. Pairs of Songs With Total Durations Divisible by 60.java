4a31da9be0fd8d6792d15508c3daa494ee4e5ed3
Qiyu Zhang <qz5uw@virginia.edu>
2022-01-01 23:59:26 -0500

    Update 1010. Pairs of Songs With Total Durations Divisible by 60.java

--- 1010. Pairs of Songs With Total Durations Divisible by 60.java
+++ 1010. Pairs of Songs With Total Durations Divisible by 60.java
@@ -1,12 +1,12 @@
-class Solution { //similar with 2 sum
-    public int numPairsDivisibleBy60(int[] time) {
-        int[] count = new int[60];
-        for (int t : time) count[t % 60]++;
+class Solution {
+    public int numPairsDivisibleBy60(int[] time) { //we just need to care about remainder of 60  
+        if (time == null || time.length == 0) return 0;
+        int[] bucket = new int[60];
+        for (int t : time) bucket[t % 60]++;
         int res = 0;
-        for (int i = 0; i <= 30; i++) { //注意这里是30，不是60否则重复
-            if (count[i] == 0) continue;
-            if (i == 0 || i == 30) res += (count[i] * (count[i] - 1) / 2); //0，30时只能自己和自己配C n 2
-            else res += count[i] * count[60 - i];
+        for (int i = 0; i <= 30; i++) { //note we need iterate to 30 to avoid duplicate, like 1, 59; 59, 1. Because 0, 30 are special, we cant directly divide 2
+            if (i == 0 || i == 30) res += bucket[i] * (bucket[i] - 1) / 2; //for 0 and 30, they are using the same. So it's pick one and another without duplicate, it's n*(n-1)/2 as C 2 n
+            else res += bucket[i] * bucket[60 - i];
         }
         return res;
     }
