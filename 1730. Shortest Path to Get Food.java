e83e4989e7173ad911235482eb5fef05d623ca6f
Qiyu Zhang <qz5uw@virginia.edu>
2022-01-04 00:53:13 -0500

    Create 1730. Shortest Path to Get Food.java

---
+++ 1730. Shortest Path to Get Food.java
@@ -0,0 +1,32 @@
+class Solution {
+    private static final int[][] dirs = new int[][] {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    public int getFood(char[][] grid) {
+        if (grid == null || grid.length == 0) return -1;
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '*') {
+                    queue.offer(new int[] {i, j});
+                    break;
+                }
+            }
+        }
+        int steps = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] cur = queue.poll();
+                if (grid[cur[0]][cur[1]] == '#') return steps;
+                grid[cur[0]][cur[1]] = 'X';
+                for (int[] dir : dirs) {
+                    int newX = cur[0] + dir[0], newY = cur[1] + dir[1];
+                    if (newX >= 0 && newX < grid.length && newY >= 0 && newY < grid[0].length && grid[newX][newY] != 'X') {
+                        queue.offer(new int[] {newX, newY});
+                    } 
+                }
+            }
+            steps++;
+        }
+        return -1;
+    }
+}
