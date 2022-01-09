75e84df86e0891bddbf741a338278d440bd4de7f
Qiyu Zhang <qz5uw@virginia.edu>
2022-01-09 16:35:31 -0500

    Update 828. Unique Letter String.java

--- 828. Unique Letter String.java
+++ 828. Unique Letter String.java
@@ -1,27 +1,31 @@
 class Solution {
-    public int uniqueLetterString(String S) {
-        if (S == null || S.length() == 0) return 0;
-        int[][] pos = new int[26][2];
-        for (int i = 0; i < 26; i++) Arrays.fill(pos[i], -1);
-        int n = S.length(), res = 0, mod = 1000000007;
-        for (int i = 0; i < n; i++) {
-            int index = S.charAt(i) - 'A';
-            res = (res + (i - pos[index][1]) * (pos[index][1] - pos[index][0])) % mod; //起始为-1，res=0
-            pos[index][0] = pos[index][1]; //更新位置
-            pos[index][1] = i;
+    public int uniqueLetterString(String s) { // if there're all unque characters
+        int pre = 0, cur = 0, sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            cur = pre + i + 1;
+            sum += cur;
+            pre = cur;
         }
-        for (int i = 0; i < 26; i++) res = (res + (n - pos[i][1]) * (pos[i][1] - pos[i][0])) % mod; 
-        //相当于计算最后每个字符出现的个数 比如ABA中第二个A
-        return res;
+        return sum;
     }
 }
-/* 找到全部substring再看每个sub有几个单独字符，这复杂度太高
-问题转换-> 如何形成只有一个单独字符的sub，然后把每个个数加起来
-举例 ABA， 针对第一个A 可以 (A)BA, (AB)A 2个
-B： A(B)A, (AB)A,  A(BA), (ABA)  4个
-A: AB(A), A(BA) 2个
-一共8个
-所以针对一个字符找出前两次出现的位置，即可计算中间那个的个数，
-corner case: 开头都为-1（因为还没出现，并且index从0 开始）
-             结尾都为n, 因为sub以n结尾
-             */
+
+class Solution {
+    public int uniqueLetterString(String s) { // if there could be depulicate
+        int pre = 0, cur = 0, sum = 0;
+        int[] lastIndex = new int[26];
+        Arrays.fill(lastIndex, -1); //mark as -1 as for the first oneit's i + 1
+        int[] contribution = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            cur = pre + (i - lastIndex[c - 'A']) - contribution[c - 'A'];
+            //it's not i + 1, but just the distance from last one to current (exclude previous one)
+            //for the previous valid substring contribution[c - 'A'], they are not going to work, like ABC, append B, then ABCB and BCB are 2 and 1, which is ABC (3) - 1 and BC(2) - 1. B's contribution is 2 
+            contribution[c - 'A'] = i - lastIndex[c - 'A'];
+            lastIndex[c - 'A'] = i;
+            sum += cur;
+            pre = cur;
+        }
+        return sum;
+    }
+}
