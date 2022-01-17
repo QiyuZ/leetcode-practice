566bf1ec40ceba74527c7c9aadf0178f87b93230
Qiyu Zhang <qz5uw@virginia.edu>
2022-01-17 15:02:42 -0500

    Update 290. Word Pattern.java

--- 290. Word Pattern.java
+++ 290. Word Pattern.java
@@ -16,3 +16,36 @@ class Solution {
         return true;
     }
 }
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split("\\s+"); //note it's \\ not //
+        if (pattern.length() != words.length) return false;
+        Map<Object, Integer> map = new HashMap<>(); //index map to record the pattern and its word's index, if the followings are not the same, then it means it cant be mapped 
+        for (int i = 0; i < pattern.length(); i++) {
+            char cur = pattern.charAt(i);
+            if (!map.containsKey(cur)) map.put(cur, i);
+            if (!map.containsKey(words[i])) map.put(words[i], i);
+            if (!map.get(cur).equals(map.get(words[i]))) return false;
+        }
+        return true;
+    }
+}
+
+
+public class Solution {
+    public boolean wordPattern(String pattern, String str) {
+        String[] words = str.split("\\s+");
+        if (words.length != pattern.length()) return false;
+        Map<Character, String> dict = new HashMap();
+        Set<String> set = new HashSet();
+        for (int i = 0; i < words.length; ++i) {
+        	char c = pattern.charAt(i);
+        	if (!dict.containsKey(c)) {
+        	    if (!set.add(words[i])) return false;
+        		dict.put(c, words[i]);
+        	} else if (!dict.get(c).equals(words[i])) return false;
+        }
+        return true;
+    }
+}
