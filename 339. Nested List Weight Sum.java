206754b4e4773f1b2b33406c46cbc6eff35db82f
Qiyu Zhang <qz5uw@virginia.edu>
2022-01-19 00:53:32 -0500

    Update 339. Nested List Weight Sum.java

--- 339. Nested List Weight Sum.java
+++ 339. Nested List Weight Sum.java
@@ -37,3 +37,22 @@ class Solution {
         return res;
     }
 }
+
+
+class Solution {
+    public int depthSum(List<NestedInteger> nestedList) {
+        Queue<NestedInteger> queue = new LinkedList<>();
+        queue.addAll(nestedList);
+        int depth = 1,total = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                NestedInteger nested = queue.poll();
+                if (nested.isInteger()) total += nested.getInteger() * depth;
+                else queue.addAll(nested.getList());
+            }
+            depth++;
+        }
+        return total;
+    }
+}
