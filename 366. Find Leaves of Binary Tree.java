0f280380e6cb93a320c484bb792294a963f33529
Qiyu Zhang <qz5uw@virginia.edu>
2022-01-23 01:08:30 -0500

    Update 366. Find Leaves of Binary Tree.java

--- 366. Find Leaves of Binary Tree.java
+++ 366. Find Leaves of Binary Tree.java
@@ -1,22 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
 class Solution {
     public List<List<Integer>> findLeaves(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
-        while (root != null) {
-            List<Integer> cur = new ArrayList<>();
-            root = helper(cur, root);
-            res.add(cur);
-        }
+        getLeavesByDepth(res, root);
         return res;
     }
-    public TreeNode helper(List<Integer> cur, TreeNode root) {
-        if (root == null) return null;
-        if (root.left == null && root.right == null) {
-            cur.add(root.val);
-            root = null;
-        } else {
-            root.left = helper(cur, root.left);
-            root.right = helper(cur, root.right);
-        }
-        return root;
+    private int getLeavesByDepth(List<List<Integer>> res, TreeNode root) { //note, it means the depth of current node not which level the node is at
+        if (root == null) return -1; //note, coz we will add 1 as res.size, so here need default to -1
+        int left = getLeavesByDepth(res, root.left), right = getLeavesByDepth(res, root.right); //from bottom to top to achieve O(n), so call method first
+        int depth = Math.max(left, right) + 1;
+        if (res.size() == depth) res.add(new ArrayList<>()); //new depth means there's no such list for it
+        res.get(depth).add(root.val);
+        root.left = root.right = null; //remove its leaves
+        return depth;
     }
 }
