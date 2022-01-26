e669e89c160320b0f64cc03715ad65560a3cfdad
Qiyu Zhang <qz5uw@virginia.edu>
2022-01-25 23:12:49 -0500

    Update 205. Isomorphic Strings.java

--- 205. Isomorphic Strings.java
+++ 205. Isomorphic Strings.java
@@ -15,3 +15,20 @@ class Solution {
         return true;
     }
 }
+
+//note, cant use one map to map 2 chars, because duplicate letters will break it, like badc,baba d doesn't exist so it sets d->b, but for t, b has been added already. 
+//So we should either check value exists or not, or use the following index check,meaning check both of 2
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.equals(t)) return true;
+        if (s.length() != t.length()) return false;
+        int[] sIndex = new int[256], tIndex = new int[256];
+        Arrays.fill(sIndex, -1); //fill with -1 as 0 is a valid index
+        Arrays.fill(tIndex, -1);
+        for (int i = 0; i < s.length(); i++) {
+            if (sIndex[s.charAt(i)] != tIndex[t.charAt(i)]) return false; //check if they have same previous letter
+            sIndex[s.charAt(i)] = tIndex[t.charAt(i)] = i; 
+        }
+        return true;
+    }
+}
