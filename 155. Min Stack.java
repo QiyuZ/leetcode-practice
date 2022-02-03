77b7c22106d0e2fd6eb6dbe57f84ae858aca306a
Qiyu Zhang <qz5uw@virginia.edu>
2022-02-02 23:51:56 -0500

    Update 155. Min Stack.java

--- 155. Min Stack.java
+++ 155. Min Stack.java
@@ -35,3 +35,45 @@ class MinStack {
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
  */
+
+class MinStack { //O(1) space - no extra space besides the container for number
+    private Stack<Long> stack;
+    private long min;
+    
+    public MinStack() {
+        stack = new Stack();
+        min = Long.MIN_VALUE;
+    }
+    
+    public void push(int val) {
+        if(stack.isEmpty()) {
+            stack.push((long)(val));
+            min = (long)val;
+        } else if(val >= min) stack.push((long)(val));
+        else {
+            stack.push((long)val + val - min);
+            min = (long)val;
+        }
+    }
+    
+    public void pop() {
+        if(stack.isEmpty()) return;
+        else if(stack.peek() >= min) stack.pop();            
+        else {
+            //Bring back the previous min
+            min = 2 * min - stack.peek();
+            stack.pop();
+        }
+        if(stack.isEmpty()) min = Long.MIN_VALUE;
+    }
+    
+    public int top() {
+        if(stack.isEmpty()) return -1;
+        else if(stack.peek() > min) return (int)(stack.peek().longValue());
+        else return (int)(min);
+    }
+    
+    public int getMin() {
+        return (int)(min);    
+    }
+}
