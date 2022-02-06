c3c52bd9277756e3b2a9c2d939c8ab475920eca9
Qiyu Zhang <qz5uw@virginia.edu>
2022-02-06 00:04:28 -0500

    Update 1554. Strings Differ by One Character.java

--- 1554. Strings Differ by One Character.java
+++ 1554. Strings Differ by One Character.java
@@ -1,13 +1,17 @@
 class Solution {
     public boolean differByOne(String[] dict) {
         Set<String> set = new HashSet<>();
+        StringBuilder sb = null; //define just one string builder to save memory
         for (String d : dict) {
+            sb = new StringBuilder(d);
             for (int i = 0; i < d.length(); i++) {
-                StringBuilder sb = new StringBuilder(d);
+                char origin = sb.charAt(i);
                 sb.setCharAt(i, '*');
                 String candidate = sb.toString();
                 if (!set.add(candidate)) return true;
+                sb.setCharAt(i, origin);
             }
+            sb.setLength(0);
         }
         return false;
     }
