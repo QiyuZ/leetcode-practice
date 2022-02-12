d2cb6cbc6c1048a57af15ebe6927a8769393e732
Qiyu Zhang <qz5uw@virginia.edu>
2022-02-12 18:43:53 -0500

    Update 1011. Capacity To Ship Packages Within D Days.java

--- 1011. Capacity To Ship Packages Within D Days.java
+++ 1011. Capacity To Ship Packages Within D Days.java
@@ -5,21 +5,25 @@ class Solution {
             left = Math.max(w, left);
             right += w;
         }
-        left = Math.max(left, right / weights.length); //最小的装货量是单次最大的或者平均重量，最大可能的就是总和，比如就一天
+        int avg = right % weights.length == 0 ? right / weights.length : (right / weights.length) + 1;
+        left = Math.max(left, avg); //最小的装货量是单次最大的或者平均重量，最大可能的就是总和，比如就一天
         while (left < right) {
             int mid = left + (right - left) / 2;
-            int day = 0, cur = 0;
-            for (int w : weights) {
-                if (cur + w > mid) { //注意要提前判断因为不能有一次cur是超过限制的
-                    day++;
-                    cur = w;
-                } else cur += w; 
-            }
-            day += 1; //最后一批货送出去
-            if (day > D) left = mid + 1;
+            int days = daysToFinish(weights, D, mid);
+            if (days > D) left = mid + 1;
             else right = mid;
         }
         return left;
     }
+    
+    private int daysToFinish(int[] weights, int D, int mid) {
+        int days = 0, cur = 0;
+        for (int w : weights) {
+            if (cur + w > mid) { //注意要提前判断因为不能有一次cur是超过限制的
+                days++;
+                cur = w;
+            } else cur += w; 
+        }
+        return ++days; //最后一批货送出去
+    }
 }
-
