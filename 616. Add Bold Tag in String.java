2568b5a06c65c3247036e44787a1e32b3134d075
Qiyu Zhang <qz5uw@virginia.edu>
2022-02-13 00:34:04 -0500

    Update 616. Add Bold Tag in String.java

--- 616. Add Bold Tag in String.java
+++ 616. Add Bold Tag in String.java
@@ -1,26 +1,26 @@
-class Solution {
+public class Solution {
     public String addBoldTag(String s, String[] dict) {
-        int n = s.length();
-        int[] mark = new int[n + 1];
-        for(String d : dict) {
-            //这里每次循环i要初始为-1，否则有时候s中没有dict也会有值
-            int i = -1;
-            //这里要用while而不是if因为会有重复的比如aaabbcc d = a,b,c,答案是和一起的如果用if会被分隔开因为有的没算上
-            while((i = s.indexOf(d, i + 1)) >= 0) {
-                mark[i]++;
-                mark[i + d.length()]--;
+        if (s == null || s.length() == 0 || dict == null || dict.length == 0) return s;
+        boolean[] bold = new boolean[s.length()];
+        int end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (String word : dict) {
+                if (s.startsWith(word, i)) end = Math.max(end, i + word.length());
             }
+            bold[i] = end > i; //if i hasn't reached to the end, we mark the current pos as true
         }
-        StringBuilder sb = new StringBuilder();
-        int sum = 0;
-        for(int i = 0; i <= n; i++) {
-            int cur = sum + mark[i];
-            if (cur > 0 && sum == 0) sb.append("<b>");
-            if (cur == 0 && sum > 0) sb.append("</b>");
-            if (i == n) break;
-            sb.append(s.charAt(i));
-            sum = cur;
+        StringBuilder result = new StringBuilder();
+        int index = 0;
+        while (index < s.length()) {
+            if (!bold[index]) {
+                result.append(s.charAt(index++));
+                continue;
+            }
+            int j = index;
+            while (j < s.length() && bold[j]) j++;
+            result.append("<b>" + s.substring(index, j) + "</b>");
+            index = j;
         }
-        return sb.toString();
+        return result.toString();
     }
 }
