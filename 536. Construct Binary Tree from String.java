c596e6145603f8222cc712f7909fa752f71765f1
Qiyu Zhang <qz5uw@virginia.edu>
2022-02-13 00:57:56 -0500

    Update 536. Construct Binary Tree from String.java

--- 536. Construct Binary Tree from String.java
+++ 536. Construct Binary Tree from String.java
@@ -4,27 +4,34 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 public class Solution {
     public TreeNode str2tree(String s) {
         if (s == null || s.length() == 0) return null;
-        int i = 0, j = 0;
-        while (j < s.length() && (Character.isDigit(s.charAt(j)) || s.charAt(j) == '-')) j++;
+        int j = 0;
+        while (j < s.length() && (Character.isDigit(s.charAt(j)) || s.charAt(j) == '-')) j++; // the first must be digits and could be negative
         TreeNode root = new TreeNode(Integer.parseInt(s.substring(0, j)));
-        if (j < s.length()) {
-            i = j;
-            int count = 1;
-            while (j < s.length() && count != 0) {
-                j++;
+        if (j < s.length()) { //left existed
+            int i = j;
+            int count = 0;
+            while (j < s.length()) {
                 if (s.charAt(j) == ')') count--;
                 if (s.charAt(j) == '(') count++;
+                if (count == 0) break; //until balanced
+                j++;
             }
-            root.left = str2tree(s.substring(i + 1, j));
+            root.left = str2tree(s.substring(i + 1, j)); // note we need i + 1 here to get rid of the first left parathesis, note we wanna exclude the last )
         }
-        if (j + 2 < s.length()) {
-            root.right = str2tree(s.substring(j + 2, s.length() - 1));
+        if (j + 2 < s.length()) { //right existed, now j is at left node's last ), so +2 is check the val of right node
+            root.right = str2tree(s.substring(j + 2, s.length() - 1)); //note it's s.length() - 1, as we wanna exclude the last )
         }
         return root;
     }
