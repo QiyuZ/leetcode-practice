62c898f3ee11dfc9018b731d809adf28a7d2836c
Qiyu Zhang <qz5uw@virginia.edu>
2022-02-13 19:32:11 -0500

    Update 926. Flip String to Monotone Increasing.java

--- 926. Flip String to Monotone Increasing.java
+++ 926. Flip String to Monotone Increasing.java
@@ -12,11 +12,23 @@ class Solution {
     }
 }
 
-public int minFlipsMonoIncr(String S) {
-  int f0 = 0, f1 = 0;
-  for (int i = 0; i < S.length(); ++i) {
-    f0 += S.charAt(i) - '0';
-    f1 = Math.min(f0, f1 + 1 - (S.charAt(i) - '0'));
-  }
-  return f1;
+/* tranverse the string
+ * use two variables to record the minimim number of flip need to make the substring from (0 - i) to be MonoIncr with end with 1 or 0;
+ * (1) for position i + 1, if preceding string is end with 1, the current string can only end with one, 
+ *   so cntEndWithOne can only be used to update the next cntEndWithOne
+ * (2) if the preceding string is end with zero, current string can either end with zero or one
+ *   so cntEndWithZero can be used to update for both next cntEndWithOne and cntEndWithZero;
+ */
+class Solution {    
+    public int minFlipsMonoIncr(String S) {
+        if (S == null || S.length() <= 1) return 0;
+        int n = S.length();
+        int cntEndWithOne = 0, cntEndWithZero = 0;
+        for (int i = 0; i < n; i++) {
+            char c = S.charAt(i);
+            cntEndWithOne = Math.min(cntEndWithZero, cntEndWithOne) + (c == '1' ? 0 : 1); //choose a min case and let this one to be one
+            cntEndWithZero += c - '0'; //let current one to be zero
+        }
+        return Math.min(cntEndWithOne, cntEndWithZero);
+    }
 }
