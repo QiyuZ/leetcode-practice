36e21692de334ab9c3bd63b143d471903642f884
Qiyu Zhang <qz5uw@virginia.edu>
2022-02-13 22:32:01 -0500

    Update 2104. Sum of Subarray Ranges.java

--- 2104. Sum of Subarray Ranges.java
+++ 2104. Sum of Subarray Ranges.java
@@ -1,3 +1,31 @@
+class Solution { //actually same as 907, but iterate twice, one for -min, another for +max
+    public long subArrayRanges(int[] A) {
+        int n = A.length, j, k;
+        long res = 0;
+        Stack<Integer> s = new Stack<>();
+        for (int i = 0; i <= n; i++) {
+            while (!s.isEmpty() && A[s.peek()] > (i == n ? Integer.MIN_VALUE : A[i])) {
+                int indexToCount = s.pop();
+                int left = indexToCount - (s.isEmpty() ? -1 : s.peek());
+                int right = i - indexToCount;
+                res -= (long)A[indexToCount] * left * right;
+            }
+            s.push(i);  
+        }
+        s.clear();
+        for (int i = 0; i <= n; i++) {
+            while (!s.isEmpty() && A[s.peek()] < (i == n ? Integer.MAX_VALUE : A[i])) {
+                int indexToCount = s.pop();
+                int left = indexToCount - (s.isEmpty() ? -1 : s.peek());
+                int right = i - indexToCount;
+                res += (long)A[indexToCount] * left * right;
+            }
+            s.push(i);
+        }
+        return res;
+    }
+}
+
 class Solution {
     public long subArrayRanges(int[] nums) {
         long res = 0;
