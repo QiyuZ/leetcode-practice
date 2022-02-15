c57b63015f8a55c4218aa7f785fc5e0898eff05d
Qiyu Zhang <qz5uw@virginia.edu>
2022-02-14 23:05:51 -0500

    Update 1570. Dot Product of Two Sparse Vectors.java

--- 1570. Dot Product of Two Sparse Vectors.java
+++ 1570. Dot Product of Two Sparse Vectors.java
@@ -43,3 +43,92 @@ class SparseVector { //read heavy
         return result;
     }
 }
+
+
+class SparseVector { //without map
+
+    List<int[]> pairs;
+
+    SparseVector(int[] nums) {
+        pairs = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                pairs.add(new int[]{i, nums[i]});
+            }
+        }
+    }
+
+    // Return the dotProduct of two sparse vectors
+    public int dotProduct(SparseVector vec) {
+        if (pairs.size() > vec.pairs.size()) return vec.dotProduct(this);
+        int result = 0, p = 0, q = 0;
+        while (p < pairs.size() && q < vec.pairs.size()) {
+            if (pairs.get(p)[0] == vec.pairs.get(q)[0]) {
+                result += pairs.get(p)[1] * vec.pairs.get(q)[1];
+                p++;
+                q++;
+            }
+            else if (pairs.get(p)[0] > vec.pairs.get(q)[0]) q++;
+            else p++;
+        }
+        return result;
+    }
+}
+
+class SparseVector { //use binary search for non-sparse
+    
+    private final List<int[]> pairs;
+    
+    // Time O(N), space O(L)
+    SparseVector(int[] nums) {
+        pairs = new ArrayList<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                pairs.add(new int[]{i, nums[i]});
+            }
+        }    
+    }
+    
+	// Return the dotProduct of two sparse vectors
+
+    public int dotProduct(SparseVector vec) {
+        if (pairs.size() <= vec.pairs.size()) {
+            return dotProduct(this, vec);
+        } else {
+            return dotProduct(vec, this);
+        }
+    }
+    
+    // Time O(min(L1, L2) * log(max(L1, L2))), space O(1)
+    private int dotProduct(SparseVector small, SparseVector large) {
+        int res = 0;
+        for (int[] pair: small.pairs) {
+            // binary search in large.pairs for pair with index pair[0]
+            int i = binarySearch(large.pairs, pair[0]);
+            if (i >= 0) {
+                res += pair[1] * large.pairs.get(i)[1];
+            }
+        }
+        return res;
+    }
+    
+    // binary search in pairs for pair with pair[0] == index
+    // return its index, if not found, return -1
+    private int binarySearch(List<int[]> pairs, int index) {
+        int l = 0;
+        int r = pairs.size() - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            int midIdx = pairs.get(mid)[0];
+            if (midIdx == index) {
+                return mid;
+            } else if (midIdx > index) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
