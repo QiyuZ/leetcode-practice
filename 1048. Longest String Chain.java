df1be65c34ec6ba28684c7ea1f540deec292a622
Qiyu Zhang <qz5uw@virginia.edu>
2022-02-19 14:40:45 -0500

    Update 1048. Longest String Chain.java

--- 1048. Longest String Chain.java
+++ 1048. Longest String Chain.java
@@ -1,23 +1,51 @@
 class Solution {
     public int longestStrChain(String[] words) {
-        Set<String> set = new HashSet<>();
-        Arrays.sort(words, (a, b) -> (a.length() - b.length()));
-        Map<String, Integer> count = new HashMap<>();
-        for (String w : words) {
-            set.add(w);
-            count.put(w, 1);
-        }
+        if (words == null || words.length == 0) return 0;
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+        int[] dp = new int[words.length];
+        int res = 0;
         for (int i = 0; i < words.length; i++) {
-            if (i > 0 && words[i].equals(words[i - 1])) continue;
-            for (int j = 0; j < words[i].length(); j++) {
-                StringBuilder sb = new StringBuilder(words[i]);
-                sb.deleteCharAt(j);
-                String next = sb.toString();
-                if (set.contains(next)) count.put(words[i], Math.max(count.get(words[i]), count.get(next) + 1));
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (valid(words[j], words[i])) dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+    
+    private boolean valid(String pre, String cur) {
+        if (pre.length() >= cur.length() || cur.length() - pre.length() > 1) return false;
+        int i = 0, j = 0;
+        boolean found = false;
+        while (i < pre.length() && j < cur.length()) {
+            if (pre.charAt(i) == cur.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                if (found) return false;
+                found = true;
+                j++;
             }
         }
+        return true;
+    }
+}
+
+class Solution {
+    public int longestStrChain(String[] words) {
+        Map<String, Integer> dp = new HashMap<>(); //word - len of seq
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
         int res = 0;
-        for (int num : count.values()) res = Math.max(res, num);
+        for (String word : words) {
+            int best = 0;
+            for (int i = 0; i < word.length(); ++i) {
+                String prev = word.substring(0, i) + word.substring(i + 1);
+                best = Math.max(best, dp.getOrDefault(prev, 0) + 1);
+            }
+            dp.put(word, best);
+            res = Math.max(res, best);
+        }
         return res;
     }
 }
