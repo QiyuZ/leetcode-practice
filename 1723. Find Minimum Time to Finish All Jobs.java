fccc86bc34bfdb009664e75ac427a81f2416a942
Qiyu Zhang <qz5uw@virginia.edu>
2022-02-21 14:37:11 -0500

    Create 1723. Find Minimum Time to Finish All Jobs.java

---
+++ 1723. Find Minimum Time to Finish All Jobs.java
@@ -0,0 +1,20 @@
+class Solution {
+    private int res = Integer.MAX_VALUE;
+    public int minimumTimeRequired(int[] jobs, int k) {
+        dfs(jobs, new int[k], jobs.length - 1, 0);
+        return res;
+    }
+    private void dfs(int[] jobs, int[] workers, int pos, int curMax) {
+        if (pos == -1) {
+            res = Math.min(curMax, res);
+            return;
+        }
+        if (curMax > res) return;
+        for (int i = 0; i < workers.length; i++) {
+            if (i > 0 && workers[i] == workers[i - 1]) continue; //reduce duplicate call
+            workers[i] += jobs[pos];
+            dfs(jobs, workers, pos - 1, Math.max(curMax, workers[i]));
+            workers[i] -= jobs[pos];
+        }
+    }
+}
