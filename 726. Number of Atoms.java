d4494620341dc617c0769e9bf00c42fecbcc7d39
Qiyu Zhang <qz5uw@virginia.edu>
2022-02-27 15:08:07 -0500

    Update 726. Number of Atoms.java

--- 726. Number of Atoms.java
+++ 726. Number of Atoms.java
@@ -1,3 +1,41 @@
+class Solution { //O(nlogn)
+    public String countOfAtoms(String formula) {
+        Map<String, Integer> res = new TreeMap<>();
+        Stack<Integer> stack = new Stack<>();
+        int multiplier = 1;
+        int i = formula.length() - 1;
+        while (i >= 0) {
+            if (formula.charAt(i) == '(') { //mutiply ends
+                multiplier /= stack.pop();
+                i--;
+                continue;
+            }
+            int end = i, val = 1;
+            if (Character.isDigit(formula.charAt(i))) {
+                while (Character.isDigit(formula.charAt(i))) i--;
+                val = Integer.parseInt(formula.substring(i+1, end+1));
+            } 
+            end = i;
+            if (Character.isLetter(formula.charAt(i))) {
+                while(Character.isLowerCase(formula.charAt(i))) i--;
+                String key = formula.substring(i, end + 1);
+                res.put(key, multiplier * val + res.getOrDefault(key, 0)); //dont change multiplier here as the value could follow the number K(SO3)2
+            } else if (formula.charAt(i) == ')') { 
+                multiplier *= val;
+                stack.push(val);
+            }
+            i--;
+        }
+        StringBuilder rs = new StringBuilder();
+        for (Map.Entry<String, Integer> entry : res.entrySet()) {
+            rs.append(entry.getKey());
+            if (entry.getValue() > 1) rs.append(entry.getValue());
+        }
+        return rs.toString();
+    }
+}
+
+
 class Solution {
     public String countOfAtoms(String formula) {
         Map<String, Integer> map = countHelper(formula);
