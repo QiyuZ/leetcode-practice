ca0bbd4a766a37a90a266dcfd5d700956518db88
Qiyu Zhang <qz5uw@virginia.edu>
2022-02-28 01:04:15 -0500

    Update 359. Logger Rate Limiter.java

--- 359. Logger Rate Limiter.java
+++ 359. Logger Rate Limiter.java
@@ -18,6 +18,26 @@ class Logger {
     }
 }
 
+class Logger {
+    private Map<String, Integer> cache;
+    public Logger() {
+        cache = new LinkedHashMap<>();
+    }
+
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        // iterator over beginning of hashmap to cleanup old entries,
+        // until we find a time < 10 seconds
+        Iterator<Map.Entry<String, Integer>> iterator = cache.entrySet().iterator();
+        while (iterator.hasNext()) {
+            if (timestamp - iterator.next().getValue() >= 10) iterator.remove();
+            else break;
+        }
+        if (cache.containsKey(message)) return false;
+        cache.put(message, timestamp);
+        return true;
+    }
+}
+
 /**
  * Your Logger object will be instantiated and called as such:
  * Logger obj = new Logger();
