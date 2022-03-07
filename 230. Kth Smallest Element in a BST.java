5554d9f8a2df2fb733314fc331329db12ec386c3
Qiyu Zhang <qz5uw@virginia.edu>
2022-03-07 00:35:44 -0500

    Update 230. Kth Smallest Element in a BST.java

--- 230. Kth Smallest Element in a BST.java
+++ 230. Kth Smallest Element in a BST.java
@@ -25,3 +25,35 @@ class Solution {
         if (node.right != null) helper(node.right);
     }
 }
+
+
+class Solution {
+    private TreeNode getPred(TreeNode current, TreeNode target){ //eg. [3,1,4,null,2] connect 2->3 find node 3 's real previous node, so it's kind like iterate from left to right
+        while(current.right != null && current.right != target) current = current.right;
+        return current;
+    }
+    public int kthSmallest(TreeNode root, int k) {
+        TreeNode current = root;
+        int count = 0;
+        while(current != null){
+            if(current.left == null){
+                count++;
+                if(count == k) return current.val;
+                current = current.right; 
+            }
+            else{
+                TreeNode pred = getPred(current.left, current);
+                if(pred.right == null){ //hasn't connect before, connect and move left
+                    pred.right = current;
+                    current = current.left;
+                } else{
+                    pred.right = null; //connect before then disconnect, same example, when visit 3 again it's connected
+                    count++;
+                    if(count == k) return current.val;
+                    current = current.right;
+                }
+            }
+        }
+        return -1;
+    }
+}
